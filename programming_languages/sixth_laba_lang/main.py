1190bb8b546efcb3ae683ff75309054777d4b3cc
c3equalz <dan.kovalev2013@gmail.com>
2023-10-01 23:36:52 +0300

    Дополнил 6 лабораторную.

--- programming_languages/sixth_laba_lang/main.py
+++ programming_languages/sixth_laba_lang/main.py
@@ -3,6 +3,7 @@
 Вариант 1 Ковалев Данил ВКБ22
 """
 import npyscreen
+import os
 
 
 class TerminalForm(npyscreen.ActionFormV2):
@@ -18,13 +19,25 @@ class TerminalForm(npyscreen.ActionFormV2):
         """
         Переопределенный метод, который позволяет
         """
-        # 1 запись, где 1 аргумент - вид виджета (текст), имя, параметры.
-        self.add(npyscreen.TitleText, name="Выполнил Ковалев Данил ВКБ22 Вариант 1", editable=False)
-        self.select_lab = self.add(npyscreen.TitleText, name="Выберите лабораторную работу: ",
-                                   begin_entry_at=30, use_two_lines=False)
 
-        # Рисуем линию, узнавая возможное пространство. Первым аргументом идет Oy, а вторым - Ox.
-        # self.draw_line_at = self.useable_space()[0] - 1
+        # Устанавливаем расположение терминала по центру
+        self.show_atx, self.show_aty = map(lambda x: (x[0] - x[1]) // 2,
+                                           zip(os.get_terminal_size(), self.useable_space()[::-1]))
+        # Добавили текст того, кто выполнил
+        self.add(npyscreen.TitleText, name="Выполнил Ковалев Данил ВКБ22 Вариант 1", editable=False)
+        self.add(npyscreen.TitleText, name="Выберите лабораторную работу: ", editable=False)
+
+        # Создаем дерево для удобного выбора лабораторной работы
+        lab_tree_data = npyscreen.TreeData(content="Лабораторные работы")
+        # Создаем узлы, где содержатся лабораторные работы
+        for i in range(1, 6):
+            parent_node = lab_tree_data.new_child(content=f"Лабораторная работа {i}")
+            for j in range(1, 5):
+                parent_node.new_child(content=f"Подзадача {j}")
+
+        self.lab_tree = self.add(npyscreen.MLTree, max_height=10, name="Выберите лабораторную работу(и):",
+                                 values=lab_tree_data, scroll_exit=True)
+        # self.lab_tree.get_selected_objects()
 
     def on_ok(self):
         ...
@@ -33,8 +46,7 @@ class TerminalForm(npyscreen.ActionFormV2):
         """
         Позволяет, чтобы выходили с помощью команды Enter, а не уничтожения приложения, используя Ctrl+C
         """
-        exiting = npyscreen.notify_yes_no("Вы хотите завершить работу программы? ")
-        if exiting:
+        if npyscreen.notify_yes_no("Вы хотите завершить работу программы? "):
             self.parentApp.setNextForm(None)
         else:
             self.parentApp.setNextForm("MAIN")
@@ -46,7 +58,7 @@ class Terminal(npyscreen.NPSAppManaged):
         Создает сущность нашего приложения. Первый аргумент - главный экран, второй - наполнение, третий - название.
         """
         npyscreen.setTheme(npyscreen.Themes.ColorfulTheme)
-        self.addForm('MAIN', TerminalForm)
+        self.addForm('MAIN', TerminalForm, name="Выполнил Ковалев Данил ВКБ22 Вариант 1")
 
 
 if __name__ == '__main__':
