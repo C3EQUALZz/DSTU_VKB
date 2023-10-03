739fcf13564f692d616a658e86ad8ec9f856a4ec
c3equalz <dan.kovalev2013@gmail.com>
2023-10-04 00:32:02 +0300

    Добавил немного улучшений в 6 лабораторную.

--- programming_languages/sixth_laba_lang/main.py
+++ programming_languages/sixth_laba_lang/main.py
@@ -1,55 +1,44 @@
 #!/usr/bin/env python
 """
 Вариант 1 Ковалев Данил ВКБ22
-TODO:
-Сделать реализацию выборов лабораторных в backend
-Заблокировать изменять доступ, когда выбрали задание.
-
 """
 import npyscreen
 import os
 
 
 class TerminalForm(npyscreen.ActionPopupWide):
-    """
-    Наследуемся от класса форм, чтобы наполнить наше приложение объектами
-    """
-
-    def __init__(self, *args, **keywords):
-        super().__init__(args, keywords)
-        self.select_lab = None
 
     def create(self):
-        """
-        Переопределенный метод, который позволяет
-        """
-
-        # Устанавливаем расположение терминала по центру
         self.show_atx, self.show_aty = map(lambda x: (x[0] - x[1]) // 2,
                                            zip(os.get_terminal_size(), self.useable_space()[::-1]))
-        # Добавили текст того, кто выполнил
-        self.add(npyscreen.TitleText, name="Выполнил Ковалев Данил ВКБ22 Вариант 1", editable=False)
         self.add(npyscreen.TitleText, name="Выберите лабораторную работу: ", editable=False)
 
-        # Создаем дерево для удобного выбора лабораторной работы
         lab_tree_data = npyscreen.TreeData(content="Лабораторные работы")
-        # Создаем узлы, где содержатся лабораторные работы
         for i in range(1, 6):
             parent_node = lab_tree_data.new_child(content=f"Лабораторная работа {i}")
             for j in range(1, 5):
                 parent_node.new_child(content=f"Подзадача {j}")
 
-        self.lab_tree = self.add(npyscreen.MLTree, max_height=6, name="Выберите лабораторную работу(и):",
+        self.lab_tree = self.add(npyscreen.MLTreeMultiSelect, max_height=8, name="Выберите лабораторную работу:",
                                  values=lab_tree_data, scroll_exit=True)
-        # self.lab_tree.get_selected_objects()
+
+        self.description = self.add(npyscreen.MultiLineEdit, value=" ", editable=False, rely=1, relx=-130)
 
     def on_ok(self):
-        ...
+        dictionary = {
+            "Лабораторная работа 1 Подзадача 1": "Напишите программу для решения примера. Предусмотрите деление на 0. "
+                                                 "\nВсе необходимые переменные вводите ниже",
+            "Лабораторная 1 Подзадача 2": "Дан произвольный список, содержащий и строки, и числа. \n"
+                                          "Выведите все четные элементы построчно. ",
+            "Лабораторная 1 Подзадача 3": "Дан произвольный список, содержащий только числа. \n "
+                                          "Выведите результат сложения чисел больше 10. "
+        }
+        question_value = (selected_node := list(self.lab_tree.get_selected_objects())[0]).content
+        parent_value = selected_node.get_parent().content
+        self.description.value = dictionary[parent_value + ' ' + question_value]
+        self.display()
 
     def on_cancel(self):
-        """
-        Позволяет, чтобы выходили с помощью команды Enter, а не уничтожения приложения, используя Ctrl+C
-        """
         if npyscreen.notify_yes_no("Вы хотите завершить работу программы? "):
             self.parentApp.setNextForm(None)
         else:
@@ -58,9 +47,6 @@ class TerminalForm(npyscreen.ActionPopupWide):
 
 class Terminal(npyscreen.NPSAppManaged):
     def onStart(self):
-        """
-        Создает сущность нашего приложения. Первый аргумент - главный экран, второй - наполнение, третий - название.
-        """
         npyscreen.setTheme(npyscreen.Themes.ColorfulTheme)
         self.addForm('MAIN', TerminalForm, name="Выполнил Ковалев Данил ВКБ22 Вариант 1")
 
--- programming_languages/sixth_laba_lang/tui_laba_1.py
+++ programming_languages/sixth_laba_lang/tui_laba_1.py
@@ -1,5 +1,25 @@
-import npyscreen
+from programming_languages.first_laba_lang.tutor import interact_with_user as interact_with_user_1
+from programming_languages.second_laba_lang.main import main as main_2
+from programming_languages.third_laba_lang.main import main as main_3
+from programming_languages.fourth_laba_lang.main import main as main_4
+from programming_languages.fifth_laba_lang.main import main as main_5
 
 
-class Lab1Form(npyscreen.ActionPopup):
-    ...
+def interact_with_user():
+    match input("Выберите лабораторную работу "):
+        case "1":
+            interact_with_user_1()
+        case "2":
+            main_2()
+        case "3":
+            main_3()
+        case "4":
+            main_4()
+        case "5":
+            main_5()
+        case _:
+            print("Вы ввели неправильный номер ")
+
+
+if __name__ == "__main__":
+    interact_with_user()
