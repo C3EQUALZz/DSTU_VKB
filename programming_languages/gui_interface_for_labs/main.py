a7485cac54b0e3ae62b9842c42f0c8e648962035
c3equalz <dan.kovalev2013@gmail.com>
2023-10-19 22:32:50 +0300

    Исправил меню

--- programming_languages/gui_interface_for_labs/main.py
+++ programming_languages/gui_interface_for_labs/main.py
@@ -88,12 +88,14 @@ class StartScreen(QtWidgets.QWidget):
     """
     Стартовое меню с выбором: настройки, старт, выход
     """
+
     def __init__(self, parent):
         super().__init__()
         # создание основного layout, где мы будем помещать остальные виджеты
         self.layout = QtWidgets.QVBoxLayout(self)
         # добавили выравнивание для каждого элемента
         self.layout.setAlignment(QtCore.Qt.AlignmentFlag.AlignHCenter | QtCore.Qt.AlignmentFlag.AlignVCenter)
+        # родительский класс
         self.parent = parent
 
         self._add_gif()
@@ -104,24 +106,29 @@ class StartScreen(QtWidgets.QWidget):
         Метод для создания кнопок в меню
         """
         start_button = QtWidgets.QPushButton("Начало")
-        start_button.setMinimumSize(200, 50)
         start_button.clicked.connect(self.parent.switch_to_main_window)
 
         settings_button = QtWidgets.QPushButton("Настройки")
-        settings_button.setMinimumSize(200, 50)
 
         exit_button = QtWidgets.QPushButton("Выход из программы")
-        exit_button.setMinimumSize(200, 50)
         exit_button.clicked.connect(self.parent.closeEvent)
 
-        for widget in (start_button, settings_button, exit_button):
-            self.layout.addWidget(widget)
+        for button in (start_button, settings_button, exit_button):
+            button.setMinimumSize(200, 50)
+            button.setStyleSheet(button.styleSheet() +
+                                 """
+                                 border-style: outset;
+                                 border-width: 1px;
+                                 border-radius: 15px;
+                                 border-color: rgb(20, 20, 20);
+                                 padding: 4px;
+                                 """)
+            self.layout.addWidget(button)
 
     def _add_gif(self):
         label = QtWidgets.QLabel()
         label.setAlignment(QtCore.Qt.AlignmentFlag.AlignHCenter | QtCore.Qt.AlignmentFlag.AlignVCenter)
         label.setMinimumSize(QtCore.QSize(200, 200))
-        label.setMaximumSize(QtCore.QSize(200, 200))
 
         movie = QtGui.QMovie("feature-open-source@2x.gif")
         label.setMovie(movie)
