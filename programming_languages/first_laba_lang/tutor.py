cb01a21b3dc21cfb6499781b1ac431829a6c19c7
c3equalz <dan.kovalev2013@gmail.com>
2023-10-19 19:41:22 +0300

    Добавил исправления в 1 лабораторной работе по яп

--- programming_languages/first_laba_lang/tutor.py
+++ programming_languages/first_laba_lang/tutor.py
@@ -5,27 +5,22 @@ from math import fabs
 def first_question(string: str) -> float | str:
     a, b, c, k = map(int, string.split())
     try:
-        # print(fabs((a ** 2 / b ** 2 + c ** 2 * a ** 2) / (a + b + c * (k - a / b ** 3)) + c + (k / b - k / a) * c))
         return fabs((a ** 2 / b ** 2 + c ** 2 * a ** 2) / (a + b + c * (k - a / b ** 3)) + c + (k / b - k / a) * c)
     except ZeroDivisionError:
-        # print("Деление на ноль")
         return "Деление на ноль"
 
 
 def second_question(lst: list[str | int]) -> str:
-    # print(*lst[1::2], sep='\n') if isinstance(lst, list) else print("Вы ввели не список")
     lst = eval(lst)
     return '\n'.join(str(x) for x in lst[1::2]) if isinstance(lst, list) else "Вы ввели не список"
 
 
 def third_question(lst: list[int]) -> int:
-    # print(sum(filter(lambda x: x > 10, map(int, lst))))
     lst = eval(lst)
     return sum(filter(lambda x: x > 10, map(int, lst)))
 
 
 def last_question(lst) -> int:
-    # print(max(map(int, lst)))
     lst = eval(lst)
     return max(map(int, lst))
 
@@ -33,14 +28,13 @@ def last_question(lst) -> int:
 def interact_with_user():
     match input("Какое задание вы хотите? Введите число от 1 до 4 "):
         case "1":
-            first_question(*map(int, (input(f"Введите {letter} ") for letter in "abck")))
-            # first_question(eval(input(" ")))
+            print(first_question(*map(int, (input(f"Введите {letter} ") for letter in "abck"))))
         case "2":
-            second_question(eval(input("Введите список, как он выглядит в repr ")))
+            print(second_question(eval(input("Введите список, как он выглядит в repr "))))
         case "3":
-            third_question(eval(input("Введите список, как он выглядит в repr ")))
+            print(third_question(eval(input("Введите список, как он выглядит в repr "))))
         case "4":
-            last_question(eval(input("Введите список, как он выглядит в repr ")))
+            print(last_question(eval(input("Введите список, как он выглядит в repr "))))
         case _:
             print("Вы выбрали неверный вариант")
 
