34435b4d6ed5ab47b28d50b0a90e13432e8ca8b3
c3equalz <dan.kovalev2013@gmail.com>
2023-11-28 00:46:19 +0300

    Начал делать загатовки по 3 лабе дискретке

---
+++ python_language/discrete_mathematics/third_laba/numpy_help_functions/tests/test_down_shift.py
@@ -0,0 +1,50 @@
+import unittest
+
+import numpy as np
+
+from ..help_functions import *
+
+
+class TestUpShiftFunction(unittest.TestCase):
+
+    def setUp(self):
+        self.matrix = np.array(
+            [
+                [1, 2, 3],
+                [4, 5, 6],
+                [7, 8, 9]
+            ]
+        )
+
+    def test_down_shift_first_column(self):
+        expected_result = np.array(
+            [
+                [7, 2, 3],
+                [1, 5, 6],
+                [4, 8, 9]
+            ])
+        self.assertTrue(np.array_equal(down_shift_column(self.matrix.copy(), 0, 1), expected_result))
+
+    def test_down_shift_second_column(self):
+        expected_result = np.array(
+            [
+                [1, 8, 3],
+                [4, 2, 6],
+                [7, 5, 9]
+            ]
+        )
+        self.assertTrue(np.array_equal(down_shift_column(self.matrix.copy(), 1, 1), expected_result))
+
+    def test_down_shift_negative_positions(self):
+        expected_result = np.array(
+            [
+                [4, 2, 3],
+                [7, 5, 6],
+                [1, 8, 9]
+            ]
+        )
+        self.assertTrue(np.array_equal(down_shift_column(self.matrix.copy(), 0, -1), expected_result))
+
+
+if __name__ == '__main__':
+    unittest.main()
