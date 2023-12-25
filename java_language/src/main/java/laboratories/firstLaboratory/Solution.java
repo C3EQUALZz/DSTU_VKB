ea399c124e6603579f1b5bb4ce67b0a823186781
C3EQUALZ <dan.kovalev2013@gmail.com>
2023-12-25 17:51:58 +0300

    Добавил до 8 задания

--- java_language/src/main/java/laboratories/firstLaboratory/Solution.java
+++ java_language/src/main/java/laboratories/firstLaboratory/Solution.java
@@ -1,7 +1,11 @@
+/**
+ * Автор: Данил Ковалев ВКБ22 Вариант -
+ */
 package laboratories.firstLaboratory;
 
 
 import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 import java.util.stream.IntStream;
@@ -12,9 +16,9 @@ import org.paukov.combinatorics3.Generator;
 import com.google.common.math.BigIntegerMath;
 
 public class Solution {
-    static final IntStream firstArray = new Random().ints(20, -100, 21);
-    static final IntStream secondArray = new Random().ints(15, -100, 16);
-    static final IntStream thirdArray = new Random().ints(10, -100, 11);
+    static final IntStream firstIntStream = new Random().ints(20, -100, 21);
+    static final IntStream secondIntStream = new Random().ints(15, -100, 16);
+    static final IntStream thirdIntStream = new Random().ints(10, -100, 11);
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -27,6 +31,8 @@ public class Solution {
             case 3 -> thirdQuestion();
             case 4 -> fourthQuestion();
             case 5 -> fifthQuestion();
+            case 6 -> sixthQuestion();
+            case 7 -> seventhQuestion();
             default -> "Вы выбрали неверное задание";
         };
         System.out.println(result);
@@ -36,12 +42,14 @@ public class Solution {
      * 1. Вычислить z = Math.exp(Math.abs(max_x)) - Math.exp(Math.abs(max_y))) / Math.sqrt((Math.abs(max_x * max_y)))
      * где - наибольший элемент массива X(20); - наибольший элемент массива Y(15).
      * Для вычисления наибольшего элемента массива использовать функцию.
+     *
      * @return значение после вычисления
      */
 
     public static double firstQuestion() {
-        var max_x = Solution.firstArray.max().orElseThrow();
-        var max_y = Solution.secondArray.max().orElseThrow();
+        // orElseTrow возвращает значение, если оно существует, в ином случае будет возмущена ошибка
+        var max_x = Solution.firstIntStream.max().orElseThrow();
+        var max_y = Solution.secondIntStream.max().orElseThrow();
 
         return (Math.exp(Math.abs(max_x)) - Math.exp(Math.abs(max_y))) / Math.sqrt(Math.abs(max_x * max_y));
     }
@@ -54,18 +62,22 @@ public class Solution {
      */
 
     public static double secondQuestion() {
-        return (Solution.firstArray.filter(n -> n > 0).sum() +
-                Solution.secondArray.filter(n -> n > 0).sum() +
-                Solution.thirdArray.filter(n -> n > 0).sum()) / 2.0;
+        // -> - это лямбда выражение, как в .net
+        return (Solution.firstIntStream.filter(n -> n > 0).sum() +
+                Solution.secondIntStream.filter(n -> n > 0).sum() +
+                Solution.thirdIntStream.filter(n -> n > 0).sum()) / 2.0;
     }
 
     /**
      * 3. Даны целые числа m, n. Вычислить с = m!/(n! * (m-n)!).
      * Для вычисления факториала использовать функцию.
+     *
      * @return Возвращает целое число, то есть результат сочетания.
      */
     public static BigInteger thirdQuestion() {
         int m = 4, n = 2;
+        // в Java, к сожалению, нет перегрузки операторов, поэтому тут математические действия делаются через методы
+        // Взято с библиотеки
         return BigIntegerMath.factorial(m).divide(
                 BigIntegerMath.factorial(n).multiply(BigIntegerMath.factorial(m - n)));
     }
@@ -76,6 +88,9 @@ public class Solution {
      * + Math.sqrt(pow_z + pow_y + Math.pow(Math.sin(z * y), 2))
      */
     public static double fourthQuestion() {
+        // Здесь я нашел библиотеку, которая генерирует комбинации из (1, 2, 3) по 2 элемента.
+        // map здесь делается то же самое, но есть небольшие разновидности, которые переделывают нам элементы к
+        // нужному типу данных. По умолчанию generator возвращает итератор, где каждый элемент - это список с числами.
         return Generator.combination(1, 2, 3).simple(2).stream().mapToDouble(x ->
                 Math.sqrt(Math.pow(x.getFirst(), 2) + Math.pow(x.getLast(), 2) +
                         Math.pow(Math.sin(x.getFirst() * x.getLast()), 2))).sum();
@@ -88,12 +103,42 @@ public class Solution {
      * @return Возвращает строку, где написаны средние значения в каждом массиве.
      */
     public static String fifthQuestion() {
-        IntStream[] arrays = {firstArray, secondArray, thirdArray};
-
+        IntStream[] arrays = {firstIntStream, secondIntStream, thirdIntStream};
+        // Нет аналога enumerate, поэтому воспользовался таким костылем.
         return IntStream.range(0, arrays.length)
                 .mapToObj(index -> "Среднее значение массива " + (index + 1) + ": " +
                         String.format("%.3f", arrays[index].filter(x -> x > 0).average().orElseThrow()))
                 .collect(Collectors.joining("\n"));
     }
+
+    /**
+     * 6. Даны массивы А(15), Y(15), C(12).
+     * Вычислить l = min(b_i) + min(c_i) if abs(min(a_i)) > 10 else 1 + min(abs(c_i))
+     */
+    public static String sixthQuestion() {
+        var result = Math.abs(firstIntStream.min().orElseThrow()) > 10 ?
+                secondIntStream.min().orElseThrow() + thirdIntStream.min().orElseThrow() :
+                1 + thirdIntStream.map(Math::abs).min().orElseThrow();
+        return String.format("Значение задания номер 6: %d", result);
+    }
+
+    /**
+     * 7. Дан массив D(40) вещественных чисел. Найти среднее геометрическое его элементов,
+     * которые удовлетворяют условию 0 < di <12. Для вычислений использовать функцию.
+     */
+    public static String seventhQuestion() {
+        double[] D = new Random().doubles(40).toArray();
+        var result = Math.pow(
+                Arrays.stream(D).filter(x -> x > 0 && x < 12).reduce(1, (a, b) -> a * b),
+                1.0 / D.length
+        );
+
+        return String.format("Значение задания номер 7: %.3f", result);
+    }
+
+    /**
+     * 8.	Дан массив А(80) целых  чисел. Найти сумму и количество теx элементов массива, которые отрицательны и нечетны. Использовать в качестве подпрограммы процедуру.
+     */
+
 }
 
