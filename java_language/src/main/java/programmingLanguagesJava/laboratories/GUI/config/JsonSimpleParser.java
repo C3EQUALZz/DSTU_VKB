646ac75a0c37495f90e2a9769c58aea0b4afc50a
c3equalz <dan.kovalev2013@gmail.com>
2024-02-12 19:24:56 +0300

    Добавил описания для json парсера.

--- java_language/src/main/java/programmingLanguagesJava/laboratories/GUI/config/JsonSimpleParser.java
+++ java_language/src/main/java/programmingLanguagesJava/laboratories/GUI/config/JsonSimpleParser.java
@@ -1,5 +1,6 @@
 /**
- * В json файле описаны
+ * В json файле описаны условия для лабораторных работ, поэтому нужно было написать соотв. парсер.
+ * Можно было сделать бд, да, но я SQL не знаю совершенно, чтобы такие вещи творить.
  */
 
 package programmingLanguagesJava.laboratories.GUI.config;
@@ -22,6 +23,8 @@ public class JsonSimpleParser {
 
     private static JSONObject parse() {
 
+        // В Java нет контекстного менеджера (по названию), но аналогичная технология называется "try with resources".
+        // Так вот, здесь тоже открывается файл на чтение, а потом в конце безопасно закрывается в блоке finally.
         try (FileReader fileReader = new FileReader("src/main/resources/laboratoriesFiles/condition.json")) {
 
             return (JSONObject) parser.parse(fileReader);
@@ -31,6 +34,12 @@ public class JsonSimpleParser {
         }
     }
 
+    /**
+     * Обратите внимание на структуру JSON файла, в котором описаны условия для лабораторных работ.
+     * @param laboratoryNumber первый ключ, с помощью которого мы получим словарь с заданиями.
+     * @param task последний ключ, с помощью которого мы получаем условие задания.
+     * @return строку с условием, где описаны все действия.
+     */
     public String get(String laboratoryNumber, String task) {
         var lab = (JSONObject) jsonObject.get(laboratoryNumber);
         return (String) lab.get(task);
