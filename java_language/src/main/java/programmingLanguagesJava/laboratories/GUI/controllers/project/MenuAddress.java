a616e784e6cc4cc376c381cf2779b3085d74e87d
matebookWindows <dan.kovalev2013@gmail.com>
2024-03-02 18:54:30 +0300

    Начал добавлять функционал

--- java_language/src/main/java/programmingLanguagesJava/laboratories/GUI/controllers/project/MenuAddress.java
+++ java_language/src/main/java/programmingLanguagesJava/laboratories/GUI/controllers/project/MenuAddress.java
@@ -3,9 +3,13 @@ package programmingLanguagesJava.laboratories.GUI.controllers.project;
 import com.sothawo.mapjfx.Coordinate;
 import com.sothawo.mapjfx.MapView;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.stage.FileChooser;
 import programmingLanguagesJava.laboratories.GUI.controllers.BaseController;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -17,9 +21,24 @@ public class MenuAddress extends BaseController {
     @FXML
     private Button downloadFile;
 
+    @FXML
+    private ComboBox<String> combobox;
+
+    private final FileChooser.ExtensionFilter photosFilter = new FileChooser.ExtensionFilter("Photo",
+            "*.jpg", "*.jpeg", "*.png");
+
+    private final FileChooser.ExtensionFilter autoCadFilesFilter = new FileChooser.ExtensionFilter("Plans",
+            "*.dwg", "*.rvt");
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         super.initialize(url, resourceBundle);
+        mapViewEvent();
+        fileChooserEvent();
+    }
+
+    private void mapViewEvent() {
         mapView.initialize();
         // Установка начальных координат и масштаба
         mapView.setCenter(new Coordinate(47.2371576587879, 39.711658338598745));
@@ -28,7 +47,17 @@ public class MenuAddress extends BaseController {
 
 
     private void fileChooserEvent() {
+        buttonConfigurator.setupButtonEvent(downloadFile, event -> {
 
+            var fileChooser = new FileChooser();
+            fileChooser.setTitle("Выберите файл с планом здания ");
+            fileChooser.getExtensionFilters().addAll(photosFilter, autoCadFilesFilter);
+
+            String downloadsPath = System.getProperty("user.home") + File.separator + "Downloads";
+            fileChooser.setInitialDirectory(new File(downloadsPath));
+
+            var selectedFile = fileChooser.showOpenDialog(((Node) event.getSource()).getScene().getWindow());
+        });
     }
 
 
