cb7e5dd7fe9974795e985c08d43168274e5ab1a3
c3equalz <dan.kovalev2013@gmail.com>
2024-03-08 14:57:49 +0300

    Исправил по читабельности класс с выбором файлов

---
+++ java_language/src/main/java/programmingLanguagesJava/laboratories/GUI/controllers/project/AdressFillingForm/fileChooserInteraction/FileChooserController.java
@@ -0,0 +1,43 @@
+/**
+ * Данная часть разработана как обработчик событий на выбор файлов, который Java сама вызывает относительно операционной системы.
+ * Здесь скорее всего будет обработка по добавлению плана здания.
+ */
+
+package programmingLanguagesJava.laboratories.GUI.controllers.project.AdressFillingForm.fileChooserInteraction;
+
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.util.List;
+
+public class FileChooserController {
+
+    // Фильтры для файлов, которые нам нужны. Я изначально планирую только фото или файлы AutoCad/Revit
+    List<FileChooser.ExtensionFilter> filters = List.of(
+            new FileChooser.ExtensionFilter("Photo", "*.jpg", "*.jpeg", "*.png"),
+            new FileChooser.ExtensionFilter("Plans", "*.dwg", "*.rvt")
+    );
+    private final FileChooser fileChooser;
+
+    /**
+     * Контроллер, который отвечает за выбор файлов путем вызова окна ОС
+     */
+    public FileChooserController() {
+        this.fileChooser = new FileChooser();
+        this.fileChooser.setTitle("Выберите файл с планом здания");
+        this.fileChooser.getExtensionFilters().addAll(filters);
+        this.fileChooser.setInitialDirectory(new File(System.getProperty("user.home") + File.separator + "Downloads"));
+    }
+
+    /**
+     * Обработчик событий выбора файла посредством OC.
+     * @param event данный event нужен, чтобы можно было вызвать окно выбора файла, ссылаясь на stage.
+     * @return возвращает путь к выбранному файлу, если пользователь выбрал
+     */
+    public String event(MouseEvent event) {
+        var selectedFile = this.fileChooser.showOpenDialog(((Node) event.getSource()).getScene().getWindow());
+        return selectedFile != null ? selectedFile.getPath() : null;
+    }
+}
