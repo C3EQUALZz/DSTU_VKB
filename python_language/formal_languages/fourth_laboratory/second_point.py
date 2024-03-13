42f6068b9b78da3ed002ef7389cb260152800c6f
c3equalz <dan.kovalev2013@gmail.com>
2024-03-13 21:28:29 +0300

    Сделал левосторонюю факторизацию

--- python_language/formal_languages/fourth_laboratory/second_point.py
+++ python_language/formal_languages/fourth_laboratory/second_point.py
@@ -1,26 +1,80 @@
-def leftFactoring(s):
-    k = []
-    l = []
-    n = ""
-    k = s.split('->')
-    l = k[1].split('/')
-    for i in range(0, len(l) - 1):
-        for j in range(0, min(len(l[i]), len(l[i + 1]))):
-            if(l[i][j] == l[i + 1][j]):
-                if l[i][j] not in n:
-                    n = n + l[i][j]
-    print(k[0] + '->' + n + "R")
-    m = k[1].split(n)
-    print("R->", end="")
-    for i in range(1, len(m)):
-        print(m[i], end="")
+"""
+Левая факторизация правил
 
-s = input("Enter the production: ")
-while True:
-    leftFactoring(s)
-    print("\ndo you have another production?")
-    T = input("y/n:")
-    if T == 'y':
-        s = input("Enter the production: ")
-    elif T == 'n':
-        break
+Полезные ресурсы: https://stackoverflow.com/questions/21217410/left-factoring-using-python
+
+---
+A -> abc
+A -> abd
+A -> aef
+B -> bcd
+B -> bde
+B -> bef
+exit
+"""
+from python_language.formal_languages.useful_functions import get_rules_from_console
+from collections import defaultdict
+
+
+def left_factorize(grammar: dict[str, list[str]]) -> dict[str, list[str]]:
+    """
+    Эта функция выполняет левую факторизацию по данной грамматике
+    Args:
+        grammar: словарь, представляющий грамматику, где ключи - это нетерминальные символы,
+         а значения - списки производственных правил.
+    Returns:
+        Left-factorized грамматику
+    """
+    new_grammar = defaultdict(list)
+
+    for non_terminal, productions in grammar.items():
+        # Найти общий префикс среди производств текущего не терминала
+        if common_prefix := _find_common_prefix(productions):
+
+            # Если существует общий префикс, создаем для него новый нетерминальный символ
+            new_non_terminal = non_terminal + "`"
+
+            # Обновляем грамматику, чтобы отразить лево-факторные правила
+            new_grammar[non_terminal].append(common_prefix + new_non_terminal)
+            new_grammar[new_non_terminal].extend([production[len(common_prefix):] for production in productions])
+
+        else:
+            new_grammar[non_terminal].append(productions)
+
+    return new_grammar
+
+
+def _find_common_prefix(productions: list[str]) -> str:
+    """
+    Находит общий префикс в списке производственных правил.
+
+    Args:
+        productions: Лист с продукциями.
+
+    Returns:
+        Общий префикс среди производственных правил.
+    """
+    if not productions:
+        return ""
+
+    common_prefix = []
+    for chars in zip(*productions):
+        # Проверяем, то что совпадают ли все символы в одной позиции
+        if len(set(chars)) == 1:
+            # Если все символы одинаковы, добавляем их к общему префиксу
+            common_prefix.append(chars[0])
+        else:
+            break
+
+    return ''.join(common_prefix)
+
+
+def main() -> None:
+    grammar = get_rules_from_console("don't_remove")
+    new_grammar = left_factorize(grammar)
+    print("Новая грамматика c факторизацией правил:")
+    print("\n".join(f"{key} -> {'|'.join(value)}".rstrip("|") for key, value in new_grammar.items()))
+
+
+if __name__ == "__main__":
+    main()
