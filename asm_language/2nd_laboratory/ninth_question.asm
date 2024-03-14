af2a1333c0eb7aac7f9525ca42133c490ac72774
matebookWindows <dan.kovalev2013@gmail.com>
2024-03-14 14:56:56 +0300

    Исправил аппаратные средства

--- asm_language/2nd_laboratory/ninth_question.asm
+++ asm_language/2nd_laboratory/ninth_question.asm
@@ -1,6 +1,6 @@
 data segment
-    array dw 17,0,12,479,-347,40,50,7,4,97
-    count dw 0
+    array dw 17,0,12,479,-347,40,50,7,4,97 ; 10 16bit elems
+    count dw 0 ; counter
 data ends
 
 code segment
@@ -10,22 +10,22 @@ start:
      mov ax, data
      mov ds, ax
 
-     lea bx, array
-     mov cx, 10
+     lea bx, array   ;; begin of array
+     mov cx, 10      ;; length of array
 
 beg:
-    mov ax, [bx]
-    cmp ax, 10h
-    jg GreaterThan10
+    mov ax, [bx]    ;; bx == pointer => data in ax
+    cmp ax, 10h     ;; compare with 10 in hex system
+    jg GreaterThan10   ;; jump if greater
 
-    jmp next
+    jmp next         ;; else
 
 GreaterThan10:
     add count, 1
 
 next:
     add bx, 2
-    loop beg
+    loop beg         ;; procedure / mark
 
 quit:
     mov ax, 4c00h
