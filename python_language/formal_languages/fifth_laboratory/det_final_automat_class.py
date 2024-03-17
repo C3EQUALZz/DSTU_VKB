1ad12768bb64e5946f5a5c18b0352e464b436217
c3equalz <dan.kovalev2013@gmail.com>
2024-03-17 22:15:31 +0300

    Начал делать ДКА

---
+++ python_language/formal_languages/fifth_laboratory/det_final_automat_class.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+
+from python_language.formal_languages.fifth_laboratory.non_det_final_automat_class import (
+    NonDeterministicFiniteAutomaton)
+
+
+class DeterministicFiniteAutomaton:
+    def __init__(self, non_deterministic_automaton: NonDeterministicFiniteAutomaton) -> None:
+        ...
