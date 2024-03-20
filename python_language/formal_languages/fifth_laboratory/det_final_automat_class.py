4c6abb32884cc9a46b6be81de2c6e6f6eb676304
c3equalz <dan.kovalev2013@gmail.com>
2024-03-21 00:21:23 +0300

    Первая версия ДКА

--- python_language/formal_languages/fifth_laboratory/det_final_automat_class.py
+++ python_language/formal_languages/fifth_laboratory/det_final_automat_class.py
@@ -20,7 +20,7 @@ F′(D, x) ={s | s∈F(t, x) для некоторого t∈D}.
 определить ДКА M′ в этих обозначениях.
 """
 import os
-from collections import defaultdict
+from collections import defaultdict, deque
 from typing import Any, Final
 
 from automata.fa.dfa import DFA
@@ -32,56 +32,72 @@ from python_language.formal_languages.fifth_laboratory.non_det_final_automat_cla
 PATH_TO_DIAGRAM: Final = os.path.join(os.path.curdir, "test_dfa.png")
 
 
-class DeterministicFiniteAutomaton:
-    def __init__(self, non_deterministic_automaton: NonDeterministicFiniteAutomaton) -> None:
-        """
-        M' = (Q', T, F', H, Z')
-        - Q' - множество состояний (set_of_states)
-        - T - множество символов входного алфавита (set_of_states)
-        - F' - функции перехода (transition_function)
-        - H - начальный символ автомата (start_state)
-        - Z` - множество заключительных состояний (final_states)
-        """
-        non_deterministic_automaton = self.__add_empty_transitions_for_nfa(non_deterministic_automaton)
+class DeterFA:
+    def __init__(self, transitions: dict[str, dict[str, set[str]]], start_state, final_states, input_alphabet, states):
+        self.start_state: str = start_state
+        self.set_of_input_alphabet_characters: set[str] = input_alphabet
+        self.set_of_states = states
+        self.transition_function = transitions
+        self.final_states = final_states
 
-        self.start_state: str = non_deterministic_automaton.start_state
-        self.set_of_input_alphabet_characters: set[str] = non_deterministic_automaton.set_of_input_alphabet_characters
-        self.set_of_states = non_deterministic_automaton.set_of_states
-        self.transition_function = non_deterministic_automaton.transition_function
-        self.final_states = non_deterministic_automaton.final_states
+    def construct_dfa(self) -> None:
+        dfa_transitions = defaultdict(dict)
+        unmarked_states = [self._epsilon_closure({self.start_state})]
+        marked_states = []
 
-    @property
-    def set_of_states(self) -> set[str]:
-        """
-        Свойство геттер, которое возвращает множество состояний
-        """
-        return self.__set_of_states
+        while unmarked_states:
+            current_state_set = unmarked_states.pop(0)
+            marked_states.append(current_state_set)
 
-    @set_of_states.setter
-    def set_of_states(self, set_of_states_from_nfa: set[str]) -> None:
-        self.__set_of_states = set_of_states_from_nfa
+            # Проверяем, содержит ли текущее состояние хотя бы одно финальное состояние
+            if current_state_set.intersection(self.final_states):
+                self.final_states.update(current_state_set)
 
-    @property
-    def transition_function(self) -> dict[str, defaultdict[Any, set]]:
-        """
-        Свойство - геттер, которую возвращает функцию переходов
-        """
-        return self.__transition_function
+            for symbol in self.set_of_input_alphabet_characters:
+                next_state_set = self._epsilon_closure(self._transition(current_state_set, symbol))
 
-    @transition_function.setter
-    def transition_function(self, transition_function_from_nfa: dict[str, defaultdict[Any, set]]) -> None:
-        self.__transition_function = transition_function_from_nfa
+                if next_state_set not in marked_states:
+                    unmarked_states.append(next_state_set)
 
-    @property
-    def final_states(self) -> set[str]:
-        """
-        Свойство - геттер, которое возвращает множество заключительных состояний
-        """
-        return self.__final_states
+                dfa_transitions[self._states_to_string(current_state_set)].update(
+                    {symbol: self._states_to_string(next_state_set)})
 
-    @final_states.setter
-    def final_states(self, final_states_from_nfa: set[str]) -> None:
-        self.__final_states = final_states_from_nfa
+        self.transition_function = dfa_transitions
+        self.set_of_states = {self._states_to_string(state) for state in marked_states}
+        # Убираем из множества финальных состояний все, которые не являются финальными в ДКА
+        self.final_states = {state for state in self.final_states if state in self.set_of_states}
+
+    def _transition(self, state_set: set[str], symbol: str) -> set[str]:
+        """
+        Данный метод используется для получения множества состояний,
+        в которые можно перейти из заданного множества состояний по заданному символу.
+
+        В частности, он принимает на вход текущее множество состояний state_set и символ symbol, и возвращает множество
+        состояний, в которые можно перейти из состояний state_set по символу symbol.
+        """
+        next_state_set = set()
+
+        for state in state_set:
+            if state in self.transition_function and symbol in self.transition_function[state]:
+                next_state_set.update(self.transition_function[state][symbol])
+
+        return next_state_set
+
+    def _epsilon_closure(self, state_set: set[str]) -> set[str]:
+        """
+        Вычисление эпсилон перехода для элемента (вершины, к которым мы можем перейти по эпислон)
+        """
+        closure = set(state_set)
+        stack = deque(state_set)
+
+        while stack:
+            state = stack.pop()
+            epsilon_states = self.transition_function.get(state, {}).get('', set())
+
+            closure.update(epsilon_states)
+            stack.extend(epsilon_states - closure)
+
+        return closure
 
     def __str__(self) -> str:
         """
@@ -106,18 +122,30 @@ class DeterministicFiniteAutomaton:
         ).show_diagram(path=PATH_TO_DIAGRAM)
 
     @staticmethod
-    def __add_empty_transitions_for_nfa(nfa: NonDeterministicFiniteAutomaton) -> NonDeterministicFiniteAutomaton:
-        for state in nfa.set_of_states:
-            if state not in nfa.transition_function:
-                nfa.transition_function.update(
-                    {state: {input_character: {} for input_character in nfa.set_of_input_alphabet_characters}})
+    def _states_to_string(state_set: set[str]) -> str:
+        """
+        Вспомогательный метод для перевода множества состояний в строку для библиотеки
+        """
+        return ''.join(sorted(state_set))
 
-        for state, functions in nfa.transition_function.items():
-            for input_character in nfa.set_of_input_alphabet_characters:
-                if input_character not in functions:
-                    nfa.transition_function[state].update({input_character: {}})
 
-        return nfa
+def main() -> None:
+    transitions = {
+        "q0": {"a": {"q0"}, "b": {"q0", "q1"}},
+        "q1": {"a": {}, "b": {"q2"}},
+        "q2": {"a": {}, "b": {}},
+    }
+
+    start_state = "q0"
+    final_states = {"q2"}
+    input_alphabet = {"a", "b"}
+    states = {"q0", "q1", "q2"}
+
+    d = DeterFA(transitions, start_state, final_states, input_alphabet, states)
+    d.construct_dfa()
+    d.show_diagram()
+
+    print(d)
 
 
 if __name__ == "__main__":
