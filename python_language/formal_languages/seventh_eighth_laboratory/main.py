a66a4be3b291c155910703a74de152e4c39ff892
c3equalz <dan.kovalev2013@gmail.com>
2024-03-26 02:52:20 +0300

    1 версия минимизации алгоритма

--- python_language/formal_languages/seventh_eighth_laboratory/main.py
+++ python_language/formal_languages/seventh_eighth_laboratory/main.py
@@ -4,23 +4,23 @@
 
 from python_language.formal_languages.seventh_eighth_laboratory.remove_unreachable_states_dfa_class import (
     RemovedUselessSymbolsDFA)
-from python_language.formal_languages.seventh_eighth_laboratory.minimize_dfa_class import DFAMinimizer
+from python_language.formal_languages.seventh_eighth_laboratory.e import DFAMinimizer
 
 
 def main() -> None:
-    states = {"A", "B", "C", "D", "E", "F", "G"}
-    alphabet = {"a", "b"}
-    start = "A"
-    final_states = {"D", "E"}
-    transitions = {
-        "A": {"a": {"B"}, "b": {"C"}},
-        "B": {"b": {"D"}},
-        "C": {"b": {"E"}},
-        "D": {"a": {"C"}, "b": {"E"}},
-        "E": {"a": {"B"}, "b": {"D"}},
-        "F": {"a": {"D"}, "b": {"G"}},
-        "G": {"a": {"F"}, "b": {"E"}}
-    }
+    # states = {"A", "B", "C", "D", "E", "F", "G"}
+    # alphabet = {"a", "b"}
+    # start = "A"
+    # final_states = {"D", "E"}
+    # transitions = {
+    #     "A": {"a": {"B"}, "b": {"C"}},
+    #     "B": {"b": {"D"}},
+    #     "C": {"b": {"E"}},
+    #     "D": {"a": {"C"}, "b": {"E"}},
+    #     "E": {"a": {"B"}, "b": {"D"}},
+    #     "F": {"a": {"D"}, "b": {"G"}},
+    #     "G": {"a": {"F"}, "b": {"E"}}
+    # }
 
     # M`` = ({'B', 'D', 'C', 'A', 'E'}, {'b', 'a'},
     # {'A': {'a': {'B'}, 'b': {'C'}},
@@ -31,8 +31,34 @@ def main() -> None:
     # A, {'D', 'E'})
 
     # A, B, C, D, E
+
+    # states = {"q0", "q1", "q2", "q3", "q4", "q5"}
+    # alphabet = {"1", "0"}
+    # start = "q0"
+    # transitions = {
+    #     "q0": {"0": {"q0"}, "1": {"q1"}},
+    #     "q1": {"0": {"q2"}, "1": {"q1"}},
+    #     "q2": {"0": {"q0"}, "1": {"q3"}},
+    #     "q3": {"0": {"q4"}, "1": {"q3"}},
+    #     "q4": {"0": {"q5"}, "1": {"q3"}},
+    #     "q5": {"0": {"q5"}, "1": {"q3"}}
+    # }
+    # final_states = {"q3", "q4", "q5"}
+
+    states = {"A", "B", "C", "D", "E"}
+    alphabet = {"0", "1"}
+    start = "A"
+    final_states = {"E"}
+    transitions = {
+        "A": {"0": {"B"}, "1": {"C"}},
+        "B": {"0": {"B"}, "1": {"D"}},
+        "C": {"0": {"B"}, "1": {"C"}},
+        "D": {"0": {"B"}, "1": {"E"}},
+        "E": {"0": {"B"}, "1": {"C"}}
+    }
+
     d = RemovedUselessSymbolsDFA(states, alphabet, start, transitions, final_states)
-    n = DFAMinimizer.from_removed_symbols_dfa(d)
+    n = DFAMinimizer(d.set_of_states, d.set_of_input_alphabet_characters, d.start_state, d.transition_function, d.final_states)
     print(n)
 
 
--- python_language/formal_languages/seventh_eighth_laboratory/minimize_dfa_class.py
+++ python_language/formal_languages/seventh_eighth_laboratory/minimize_dfa_class.py
@@ -2,9 +2,12 @@
 Минимизация самого ДКА, алгоритм 2
 Полезные ресурсы:
 - https://youtu.be/v4YIiRfnnDY?si=Iq5-yuFR7Nrke5QX
+- https://youtu.be/0XaGAkY09Wc?si=Ltcw-JBfBYO82KMQ
 """
 import os
-from typing import Self, Set, AnyStr, Mapping, Final
+from typing import Self, Set, AnyStr, Mapping, Final, Tuple, List
+from copy import deepcopy
+from collections import defaultdict
 
 from python_language.formal_languages.seventh_eighth_laboratory.remove_unreachable_states_dfa_class import (
     RemovedUselessSymbolsDFA)
@@ -21,12 +24,13 @@ class DFAMinimizer:
                  start: AnyStr,
                  transitions: Mapping[AnyStr, Mapping[AnyStr, Set[AnyStr]]],
                  final_states: Set[AnyStr]) -> None:
+
         self.start_state = start
         self.set_of_input_alphabet_characters: set[str] = alphabet
         self.set_of_states = states
         self.transition_function = transitions
         self.final_states = final_states
-        self.__minimize()
+        self.__minimize_dfa()
 
     @classmethod
     def from_removed_symbols_dfa(cls, dfa: RemovedUselessSymbolsDFA) -> Self:
@@ -38,8 +42,94 @@ class DFAMinimizer:
             final_states=dfa.final_states
         )
 
-    def __minimize(self) -> None:
-        ...
+    def __minimize_dfa(self) -> None:
+        """
+        Здесь происходит логика минимизации ДКА
+        """
+        self.set_of_states = {"".join(s) for s in self.__minimize_states()}
+        self.final_states = {state for state in self.set_of_states if set(state) & self.final_states}
+        self.start_state = ''.join([state for state in self.set_of_states if self.start_state in state])
+        self.transition_function = self.__construct_transition_function()
+
+    def __minimize_states(self) -> list[Set[AnyStr]]:
+        """
+        Здесь происходит запуск логики для минимизации вершин графа
+        """
+        eq_class = [sorted(self.final_states), sorted(self.set_of_states - self.final_states)]
+
+        while True:
+
+            new_eq_class = self.__new_class_eq(deepcopy(eq_class))
+
+            if eq_class == new_eq_class:
+                break
+
+            eq_class = new_eq_class
+
+        return new_eq_class
+
+    def __new_class_eq(self, eq_class: list[Set[AnyStr]]) -> list[Set[AnyStr]]:
+        """
+        Здесь формируется каждый раз новый класс эквивалентности (0, 1, 2 и т.п).
+        Подробнее смотрите данное видео:
+        - https://youtu.be/0XaGAkY09Wc?si=GpCJOqZkFvVLfGb5&t=920
+
+        Args:
+            eq_class (list[set[str]]) - прошлый класс эквивалентности
+        Returns:
+            list[set[AnyStr]] - новый класс эквивалентности
+        """
+        new_eq_class = []
+        for group in eq_class:
+
+            if len(group) == 1:
+                new_eq_class.append(group)
+            else:
+                new_eq_class.extend(filter(None, self.__create_new_group_based_on_the_old(group)))
+
+        return new_eq_class
+
+    def __create_new_group_based_on_the_old(self,
+                                            group: set[AnyStr]
+                                            ) -> tuple[set[AnyStr], set[AnyStr]]:
+        """
+        Здесь вот каждый раз создаются новые группы - наборы вершин
+        Args:
+            group - группа, которую мы хотим видоизменить
+        Returns:
+            Кортеж из новой и старой видоизмененной группы
+            Пример: ({A, B, C} -> __create_new_group_based_on_the_old -> {A, B}, {C})
+        """
+
+        new_group = set()
+        remaining_states = group.copy()
+
+        for state in group:
+            # Проверяем все ли переходы из этого состояния остаются в группе
+            if all(self.transition_function.get(state, {}).get(symbol, set()).issubset(group)
+                   for symbol in self.set_of_input_alphabet_characters):
+                new_group.add(state)
+                remaining_states.remove(state)
+
+        return remaining_states, new_group
+
+    def __construct_transition_function(self) -> Mapping[AnyStr, Mapping[AnyStr, Set[AnyStr]]]:
+        """
+        Перестроение таблицы переходов на основе наших вершин.
+        На основе старой берутся вершины и объединяются в общую функцию перехода.
+        """
+        new_transition_function = defaultdict(dict)
+
+        for state in self.set_of_states:
+            for symbol in self.set_of_input_alphabet_characters:
+                transitions = set()
+                for old_state in state:
+                    transitions.update(self.transition_function.get(old_state, {}).get(symbol, []))
+
+                new_transition_function[state][symbol] = {new_state_set for new_state_set in self.set_of_states if
+                                                          any(new_state in new_state_set for new_state in transitions)}
+
+        return new_transition_function
 
     def __str__(self) -> str:
         """
