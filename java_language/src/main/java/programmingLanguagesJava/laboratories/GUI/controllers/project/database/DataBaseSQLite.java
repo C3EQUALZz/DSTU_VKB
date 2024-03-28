e6da1ec7f6a140814aa6bf2fb607e52d81df9af1
matebookWindows <dan.kovalev2013@gmail.com>
2024-03-28 15:41:15 +0300

    Начал делать взаимодействие с БД

---
+++ java_language/src/main/java/programmingLanguagesJava/laboratories/GUI/controllers/project/database/DataBaseSQLite.java
@@ -0,0 +1,65 @@
+package programmingLanguagesJava.laboratories.GUI.controllers.project.database;
+
+public class DataBaseSQLite implements DAO {
+
+
+    @Override
+    public void addAddress(String address) {
+
+    }
+
+    @Override
+    public void addFullName(String fullName) {
+
+    }
+
+    @Override
+    public void addMainPerson(String person) {
+
+    }
+
+    @Override
+    public void addBuildingPlan(String pathToBuildingPlan) {
+
+    }
+
+    @Override
+    public void addAllResponsiblePeople(String persons) {
+
+    }
+
+    @Override
+    public void addContractDocument(String pathToDocument) {
+
+    }
+
+    @Override
+    public String getAddress() {
+        return null;
+    }
+
+    @Override
+    public String getFullName() {
+        return null;
+    }
+
+    @Override
+    public String getMainPerson() {
+        return null;
+    }
+
+    @Override
+    public String getBuildingPlan() {
+        return null;
+    }
+
+    @Override
+    public String getAllResponsiblePeople() {
+        return null;
+    }
+
+    @Override
+    public String getContractDocument() {
+        return null;
+    }
+}
