47d04851831741d24d86956592190c58b9f1dcdd
c3equalz <dan.kovalev2013@gmail.com>
2024-04-07 11:48:47 +0300

    Вынес в отдельный класс настройку Scene

--- java_language/src/main/java/programmingLanguagesJava/laboratories/GUI/controllers/project/fileObserver/FileReaderService.java
+++ java_language/src/main/java/programmingLanguagesJava/laboratories/GUI/controllers/project/fileObserver/FileReaderService.java
@@ -5,11 +5,20 @@ import java.io.FileReader;
 import java.io.IOException;
 import lombok.Getter;
 
+/**
+ * Класс, чтобы считывать информацию из файла, которая нужна для обнаружения.
+ * Он не каждый раз запускается для считывания, можно было один раз по идее ввод открывать,
+ * но я хочу уже быстрее сдать проект
+ */
 @Getter
 class FileReaderService {
 
     private final String FILE_PATH = System.getProperty("user.dir") + "/src/main/resources/projectFiles/fileWatch.txt";
 
+    /**
+     * Метод, который считывает информацию построчно из файла. Каждый раз читает одну строку
+     * @return возвращает информацию с одной строки.
+     */
     public String readWordFromFile() {
 
         try (var reader = new BufferedReader(new FileReader(FILE_PATH))) {
