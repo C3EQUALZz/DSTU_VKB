5b6fee8599fed43588b35916e495a3ed6996d675
c3equalz <dan.kovalev2013@gmail.com>
2024-05-01 23:52:13 +0300

    Добавил тесты, но код не валидный

--- python_language/formal_languages/seventh_eighth_laboratory/main.py
+++ python_language/formal_languages/seventh_eighth_laboratory/main.py
@@ -8,7 +8,24 @@ from python_language.formal_languages.seventh_eighth_laboratory.minimize_dfa_cla
 
 
 def main() -> None:
-    ...
+
+    states = {"q0", "q1", "q2", "q3", "q4", "q5", "q6", "q7"}
+    alphabet = {"a", "b"}
+    start = "q0"
+    final_states = {"q2", "q3", "q5", "q6", "q7"}
+    transitions = {
+        "q0": {"a": {"q1"}, "b": {"q4"}},
+        "q1": {"a": {"q2"}, "b": {"q4"}},
+        "q2": {"a": {"q3"}, "b": {"q5"}},
+        "q3": {"a": {"q6"}, "b": {"q3"}},
+        "q4": {"a": {"q5"}, "b": {"q1"}},
+        "q5": {"a": {"q3"}, "b": {"q4"}},
+        "q6": {"a": {"q3"}, "b": {"q6"}},
+        "q7": {"a": {"q3"}, "b": {"q6"}},
+    }
+    d = RemovedUselessSymbolsDFA(states, alphabet, start, transitions, final_states)
+    n = DFAMinimizer.from_removed_symbols_dfa(d)
+    n.show_diagram()
 
 
 if __name__ == "__main__":
--- python_language/formal_languages/seventh_eighth_laboratory/test.py
+++ python_language/formal_languages/seventh_eighth_laboratory/test.py
@@ -4,6 +4,38 @@ from python_language.formal_languages.seventh_eighth_laboratory.remove_unreachab
 from python_language.formal_languages.seventh_eighth_laboratory.minimize_dfa_class import DFAMinimizer
 
 
+def test_twelfth_variant():
+    """
+    Вариант Паши
+    """
+    states = {"q0", "q1", "q2", "q3", "q4", "q5", "q6"}
+    alphabet = {"a", "b"}
+    start = "q0"
+    final_states = {"q3", "q6"}
+    transitions = {
+        "q0": {"a": {"q1"}, "b": {"q4"}},
+        "q1": {"a": {"q2"}, "b": {"q1"}},
+        "q2": {"a": {"q3"}, "b": {"q6"}},
+        "q3": {"a": {"q3"}, "b": {"q3"}},
+        "q4": {"a": {"q5"}, "b": {"q1"}},
+        "q5": {"a": {"q6"}, "b": {"q3"}},
+        "q6": {"a": {"q6"}, "b": {"q3"}},
+    }
+
+    d = RemovedUselessSymbolsDFA(states, alphabet, start, transitions, final_states)
+    n = DFAMinimizer.from_removed_symbols_dfa(d)
+
+    assert n.set_of_states == {'q0', 'q1q4', 'q2q5', 'q3q6'}
+    assert n.set_of_input_alphabet_characters == {'a', 'b'}
+    assert n.transition_function == {
+        'q0': {'a': {'q1q4'}, 'b': {'q1q4'}},
+        'q1q4': {'a': {'q2q5'}, 'b': {'q1q4'}},
+        'q2q5': {'a': {'q3q6'}, 'b': {'q3q6'}},
+        'q3q6': {'a': {'q3q6'}, 'b': {'q3q6'}},
+    }
+    assert n.final_states == {"q3q6"}
+
+
 def test_fourth_variant():
     """
     Вариант Снежаны
@@ -40,7 +72,7 @@ def test_fourth_variant():
 
 def test_fourteenth_variant():
     """
-    Вариант Коли
+    14 вариант
     """
 
     states = {"q0", "q1", "q2", "q3", "q4", "q5", "q6"}
