ccec9bca3a37b520757c6212aa4aee29020f4e7b
c3equalz <dan.kovalev2013@gmail.com>
2024-06-19 18:33:50 +0300

    полностью готовая 1 лабораторная по C#

--- csharp_language/ProrgrammingLanguages/FifthSemester/WinFormsAppForLaboratories/Laboratories/FirstLaboratory/ThirdQuestion/TaskForm.Designer.cs
+++ csharp_language/ProrgrammingLanguages/FifthSemester/WinFormsAppForLaboratories/Laboratories/FirstLaboratory/ThirdQuestion/TaskForm.Designer.cs
@@ -28,12 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "TaskForm";
+            MdiMenu = new MenuStrip();
+            FileMenuItem = new ToolStripMenuItem();
+            NewMenuItem = new ToolStripMenuItem();
+            ExitMenuItem = new ToolStripMenuItem();
+            WindowMenuItem = new ToolStripMenuItem();
+            WindowCascadeMenuItem = new ToolStripMenuItem();
+            WindowTileMenuItem = new ToolStripMenuItem();
+            MdiMenu.SuspendLayout();
+            SuspendLayout();
+            // 
+            // MdiMenu
+            // 
+            MdiMenu.ImageScalingSize = new Size(20, 20);
+            MdiMenu.Items.AddRange(new ToolStripItem[] { FileMenuItem, WindowMenuItem });
+            MdiMenu.Location = new Point(0, 0);
+            MdiMenu.MdiWindowListItem = WindowMenuItem;
+            MdiMenu.Name = "MdiMenu";
+            MdiMenu.Size = new Size(402, 28);
+            MdiMenu.TabIndex = 1;
+            MdiMenu.Text = "menuStrip1";
+            // 
+            // FileMenuItem
+            // 
+            FileMenuItem.DropDownItems.AddRange(new ToolStripItem[] { NewMenuItem, ExitMenuItem });
+            FileMenuItem.Name = "FileMenuItem";
+            FileMenuItem.Size = new Size(46, 24);
+            FileMenuItem.Text = "&File";
+            // 
+            // NewMenuItem
+            // 
+            NewMenuItem.Name = "NewMenuItem";
+            NewMenuItem.Size = new Size(224, 26);
+            NewMenuItem.Text = "&New";
+            NewMenuItem.Click += NewMenuItem_Click;
+            // 
+            // ExitMenuItem
+            // 
+            ExitMenuItem.Name = "ExitMenuItem";
+            ExitMenuItem.Size = new Size(122, 26);
+            ExitMenuItem.Text = "&Exit";
+            ExitMenuItem.Click += ExitMenuItem_Click;
+            // 
+            // WindowMenuItem
+            // 
+            WindowMenuItem.DropDownItems.AddRange(new ToolStripItem[] { WindowCascadeMenuItem, WindowTileMenuItem });
+            WindowMenuItem.Name = "WindowMenuItem";
+            WindowMenuItem.Size = new Size(78, 24);
+            WindowMenuItem.Text = "&Window";
+            // 
+            // WindowCascadeMenuItem
+            // 
+            WindowCascadeMenuItem.Name = "WindowCascadeMenuItem";
+            WindowCascadeMenuItem.Size = new Size(224, 26);
+            WindowCascadeMenuItem.Text = "&Cascade";
+            WindowCascadeMenuItem.Click += WindowCascadeMenuItem_Click;
+            // 
+            // WindowTileMenuItem
+            // 
+            WindowTileMenuItem.Name = "WindowTileMenuItem";
+            WindowTileMenuItem.Size = new Size(224, 26);
+            WindowTileMenuItem.Text = "&Tile";
+            WindowTileMenuItem.Click += WindowTileMenuItem_Click;
+            // 
+            // ParentForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(402, 273);
+            Controls.Add(MdiMenu);
+            IsMdiContainer = true;
+            MainMenuStrip = MdiMenu;
+            Name = "ParentForm";
+            Text = "Parent Form";
+            MdiMenu.ResumeLayout(false);
+            MdiMenu.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip MdiMenu;
+        private ToolStripMenuItem FileMenuItem;
+        private ToolStripMenuItem NewMenuItem;
+        private ToolStripMenuItem ExitMenuItem;
+        private ToolStripMenuItem WindowMenuItem;
+        private ToolStripMenuItem WindowCascadeMenuItem;
+        private ToolStripMenuItem WindowTileMenuItem;
     }
 }
--- csharp_language/ProrgrammingLanguages/FifthSemester/WinFormsAppForLaboratories/Laboratories/FirstLaboratory/ThirdQuestion/TaskForm.cs
+++ csharp_language/ProrgrammingLanguages/FifthSemester/WinFormsAppForLaboratories/Laboratories/FirstLaboratory/ThirdQuestion/TaskForm.cs
@@ -1,20 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿namespace WinFormsAppForLaboratories.Laboratories.FirstLaboratory.ThirdQuestion;
 
-namespace WinFormsAppForLaboratories.Laboratories.FirstLaboratory.ThirdQuestion
+public partial class TaskForm : Form
 {
-    public partial class TaskForm : Form
+    private int openDocuments = 0;
+    public TaskForm()
     {
-        public TaskForm()
+        InitializeComponent();
+    }
+
+    private void ExitMenuItem_Click(object sender, EventArgs e)
+    {
+        Close();
+    }
+
+    private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+    {
+        LayoutMdi(MdiLayout.Cascade);
+    }
+
+    private void WindowTileMenuItem_Click(object sender, EventArgs e)
+    {
+        LayoutMdi(MdiLayout.TileHorizontal);
+    }
+
+    private void NewMenuItem_Click(object sender, EventArgs e)
+    {
+        ChildForm newChild = new()
         {
-            InitializeComponent();
-        }
+            MdiParent = this
+        };
+        newChild.Text = newChild.Text + " " + ++openDocuments;
+
+        newChild.Show();
     }
 }
