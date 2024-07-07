1082b886ae14c4f4b9a79ca67583727a5a120297
Никита Фомин <defdXs@yandex.ru>
2024-07-07 20:12:42 +0300

    Added comment field to Contract entity in database

---
+++ csharp_language/ProrgrammingLanguages/FifthSemester/Project/Shield.Web/Migrations/20240707171132_ContractComments.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shield.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContractComments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comment",
+                table: "Contracts",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comment",
+                table: "Contracts");
+        }
+    }
+}
