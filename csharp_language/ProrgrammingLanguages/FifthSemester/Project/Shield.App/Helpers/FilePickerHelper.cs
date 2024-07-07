2351d54e167bb64a2da281b545c60155be0792ce
Никита Фомин <defdXs@yandex.ru>
2024-07-07 19:01:40 +0300

    Fixed "Invalid window handler" error

--- csharp_language/ProrgrammingLanguages/FifthSemester/Project/Shield.App/Helpers/FilePickerHelper.cs
+++ csharp_language/ProrgrammingLanguages/FifthSemester/Project/Shield.App/Helpers/FilePickerHelper.cs
@@ -1,4 +1,5 @@
-﻿using Windows.Storage;
+﻿using Microsoft.UI.Xaml.Controls;
+using Windows.Storage;
 using Windows.Storage.Pickers;
 
 namespace Shield.App.Helpers;
@@ -11,13 +12,16 @@ class FilePickerHelper
     /// <param name="fileTypes"></param>
     public static async Task<StorageFile?> PickSingleFile(string fileTypes = "jpg|jpeg|png", PickerLocationId suggestedStartLocation = PickerLocationId.Desktop, PickerViewMode viewMode = PickerViewMode.Thumbnail)
     {
-        var openPicker = new FileOpenPicker();
+        var filePicker = new FileOpenPicker();
         
-        openPicker.ViewMode = viewMode;
-        openPicker.SuggestedStartLocation = suggestedStartLocation;
+        filePicker.ViewMode = viewMode;
+        filePicker.SuggestedStartLocation = suggestedStartLocation;
         
-        fileTypes.Split('|').ToList().ForEach(type => openPicker.FileTypeFilter.Add($".{type}"));
-        
-        return await openPicker.PickSingleFileAsync();
+        fileTypes.Split('|').ToList().ForEach(type => filePicker.FileTypeFilter.Add($".{type}"));
+
+        var hwnd = App.MainWindow.GetWindowHandle();
+        WinRT.Interop.InitializeWithWindow.Initialize(filePicker, hwnd);
+
+        return await filePicker.PickSingleFileAsync();
     }
 }
