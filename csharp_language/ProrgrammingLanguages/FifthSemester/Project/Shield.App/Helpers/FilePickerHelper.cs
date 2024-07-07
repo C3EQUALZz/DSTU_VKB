77d233521073ab006819f8bae04e0c1c8b70d90f
Никита Фомин <defdXs@yandex.ru>
2024-07-07 18:31:28 +0300

    *Now* truly added FilePickerHelper

---
+++ csharp_language/ProrgrammingLanguages/FifthSemester/Project/Shield.App/Helpers/FilePickerHelper.cs
@@ -0,0 +1,23 @@
+﻿using Windows.Storage;
+using Windows.Storage.Pickers;
+
+namespace Shield.App.Helpers;
+
+class FilePickerHelper
+{
+    /// <summary>
+    /// Accepts file types in format "jpg|jpeg|png"
+    /// </summary>
+    /// <param name="fileTypes"></param>
+    public static async Task<StorageFile?> PickSingleFile(string fileTypes = "jpg|jpeg|png", PickerLocationId suggestedStartLocation = PickerLocationId.Desktop, PickerViewMode viewMode = PickerViewMode.Thumbnail)
+    {
+        var openPicker = new FileOpenPicker();
+        
+        openPicker.ViewMode = viewMode;
+        openPicker.SuggestedStartLocation = suggestedStartLocation;
+        
+        fileTypes.Split('|').ToList().ForEach(type => openPicker.FileTypeFilter.Add($".{type}"));
+        
+        return await openPicker.PickSingleFileAsync();
+    }
+}
