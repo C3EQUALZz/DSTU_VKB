41b0b9d716a02e621c6e55c19cf7942416d05e31
Никита Фомин <defdXs@yandex.ru>
2024-07-12 16:30:34 +0300

    Applied SignDate field and nullable Owners & Comment

--- csharp_language/ProrgrammingLanguages/FifthSemester/Project/Shield.Web/Migrations/20240712123204_SignDate.Designer.cs
+++ csharp_language/ProrgrammingLanguages/FifthSemester/Project/Shield.Web/Migrations/20240712123204_SignDate.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Shield.Web.Data.Contexts;
 
@@ -10,9 +11,11 @@ using Shield.Web.Data.Contexts;
 namespace Shield.Web.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240712123204_SignDate")]
+    partial class SignDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.6");
