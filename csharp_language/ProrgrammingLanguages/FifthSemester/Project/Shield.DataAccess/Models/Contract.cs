ce32004622453a755d9f8a479950126f049cd987
Никита Фомин <defdXs@yandex.ru>
2024-07-12 16:29:44 +0300

    Added SignDate field; Owners & Comment are nullable now

--- csharp_language/ProrgrammingLanguages/FifthSemester/Project/Shield.DataAccess/Models/Contract.cs
+++ csharp_language/ProrgrammingLanguages/FifthSemester/Project/Shield.DataAccess/Models/Contract.cs
@@ -6,9 +6,10 @@ public class Contract
 {
     public int ContractId { get; set; }
     public string Address { get; set; }
-    public string Owners { get; set; }
+    public string? Owners { get; set; }
     public string Bailee { get; set; }
-    public string Comment { get; set; }
+    public string? Comment { get; set; }
+    public DateOnly SignDate { get; set; }
 
     [ForeignKey("Plan")]
     public int PlanId { get; set; }
