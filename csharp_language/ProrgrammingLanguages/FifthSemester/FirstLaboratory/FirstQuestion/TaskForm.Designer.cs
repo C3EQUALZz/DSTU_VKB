53034d27baffc47ecf798e370a58dd42d8b20bef
c3equalz <dan.kovalev2013@gmail.com>
2024-08-21 17:02:12 +0300

    Перенес 1 задание в 1 лабораторной

---
+++ csharp_language/ProrgrammingLanguages/FifthSemester/FirstLaboratory/FirstQuestion/TaskForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace FirstLaboratory.FirstQuestion
+{
+    partial class TaskForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            ExitButton = new Button();
+            SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            ExitButton.Location = new Point(357, 216);
+            ExitButton.Margin = new Padding(3, 4, 3, 4);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(226, 104);
+            ExitButton.TabIndex = 0;
+            ExitButton.Text = "Закрыть приложение";
+            ExitButton.UseVisualStyleBackColor = true;
+            ExitButton.Click += ExitButton_Click;
+            // 
+            // TaskForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(914, 600);
+            Controls.Add(ExitButton);
+            FormBorderStyle = FormBorderStyle.None;
+            Margin = new Padding(3, 4, 3, 4);
+            Name = "TaskForm";
+            Text = "TaskForm";
+            Paint += TaskForm_Paint;
+            Resize += TaskForm_Resize;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button ExitButton;
+    }
+}
---
+++ csharp_language/ProrgrammingLanguages/FifthSemester/FirstLaboratory/FirstQuestion/TaskForm.cs
@@ -0,0 +1,63 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace FirstLaboratory.FirstQuestion
+{
+    public partial class TaskForm : Form
+    {
+        private readonly Pen borderPen = new(Color.Black, 3);
+        private readonly EllipseRegion ellipseRegion;
+
+        public TaskForm()
+        {
+            InitializeComponent();
+            BackColor = Color.Red;
+            ellipseRegion = new EllipseRegion(this);
+        }
+
+        /// <summary>
+        /// Обрабатывает событие изменения размера формы.
+        /// </summary>
+        private void TaskForm_Resize(object sender, EventArgs e)
+        {
+            ellipseRegion?.Draw();
+        }
+
+        /// <summary>
+        /// Обрабатывает событие перерисовки формы.
+        /// </summary>
+        /// <param name="e">Объект <see cref="PaintEventArgs"/>, содержащий данные события.</param>
+        private void TaskForm_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawEllipse(borderPen, 0, 0, Width - 1, Height - 1);
+        }
+
+        /// <summary>
+        /// Закрытие приложения
+        /// </summary>
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        
+    }
+
+    /// <summary>
+    /// Управляет эллиптическим регионом формы.
+    /// </summary>
+    class EllipseRegion(Form form)
+    {
+        private readonly Form form = form ?? throw new ArgumentNullException(nameof(form));
+        private readonly GraphicsPath path = new();
+
+        /// <summary>
+        /// Обновляет регион формы до эллипса. 
+        /// </summary>
+        public void Draw()
+        {
+            path.Reset();
+            path.AddEllipse(0, 0, form.Width, form.Height);
+            form.Region = new Region(path);
+        }
+    }
+}
