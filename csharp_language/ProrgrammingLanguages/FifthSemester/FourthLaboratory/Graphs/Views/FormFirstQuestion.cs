c49137f8618801663b8a37c4190ecafd2f7f2065
c3equalz <dan.kovalev2013@gmail.com>
2024-08-31 01:25:31 +0300

    Добавил формы для 20 вариантов

---
+++ csharp_language/ProrgrammingLanguages/FifthSemester/FourthLaboratory/Graphs/Views/FormFirstQuestion.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnPaint.Graphs.Views
+{
+    public partial class FormFirstQuestion : Form
+    {
+        public FormFirstQuestion()
+        {
+            InitializeComponent();
+        }
+    }
+}
