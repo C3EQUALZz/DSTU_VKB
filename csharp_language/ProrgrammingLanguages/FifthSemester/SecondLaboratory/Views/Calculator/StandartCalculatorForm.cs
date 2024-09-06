c3c15bf417bb6eff65c9977f0ed6b4ebf8cebb97
Никита Фомин <defdXs@yandex.ru>
2024-09-06 15:42:55 +0300

    Tried making buttons round (not work)

--- csharp_language/ProrgrammingLanguages/FifthSemester/SecondLaboratory/Views/Calculator/StandartCalculatorForm.cs
+++ csharp_language/ProrgrammingLanguages/FifthSemester/SecondLaboratory/Views/Calculator/StandartCalculatorForm.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Forms;
+﻿using SecondLaboratory.Extensions;
+using System.Windows.Forms;
 
 namespace SecondLaboratory.Views.Calculator
 {
@@ -13,6 +14,45 @@ namespace SecondLaboratory.Views.Calculator
         {
             OperationHistoryLabel.Text = string.Empty;
             OperationLabel.Text = "0";
+
+            // NOT WORKING
+            //RoundButtons();
+        }
+
+        // HELP
+        private void RoundButtons()
+        {
+            Button[] buttons = [
+                    PercentButton,
+                    CEButton,
+                    CButton,
+                    BackspaceButton,
+                    InvertButton,
+                    SquareButton,
+                    SquareRootButton,
+                    DivisionButton,
+                    SevenButton,
+                    EightButton,
+                    NineButton,
+                    MultiplicationButton,
+                    FourButton,
+                    FiveButton,
+                    SixButton,
+                    MinusButton,
+                    OneButton,
+                    TwoButton,
+                    ThreeButton,
+                    PlusButton,
+                    PMButton,
+                    ZeroButton,
+                    PointButton,
+                    EqualsButton
+            ];
+
+            foreach (var button in buttons)
+            {
+                button.SetRoundedShape(10);
+            }
         }
     }
 }
