f393f3f772b034bf0a46f80c27d6b76b3c9ee644
c3equalz <dan.kovalev2013@gmail.com>
2024-09-09 22:25:20 +0300

    Обновил форматирование

--- asm_language/6th_laboratory/seventh_question/second_half.asm
+++ asm_language/6th_laboratory/seventh_question/second_half.asm
@@ -3,7 +3,7 @@ data segment
     prompt2 db 13,10,'Enter substring: '
     crlf db 13,10,'$'
     txtno db 13,10,'Not '
-    txtyes db 'exists $'
+    txtyes db 'exists $'
     buf1 db 255
     len1 db ?
     str1 db 255 dup (?)
@@ -40,57 +40,57 @@ code segment
         mov counter,ch
         lea di,str1
 
-        m1:
-            push cx
-            push di
-            lea si,str2
-            mov cl,len2
-            xor ch,ch
-            repe cmpsb
-            jne m2
-            inc byte ptr [counter]
+    m1:
+        push cx
+        push di
+        lea si,str2
+        mov cl,len2
+        xor ch,ch
+        repe cmpsb
+        jne m2
+        inc byte ptr [counter]
 
-        m2:
-            pop di
-            inc di
-            pop cx
-            loop m1
-            cmp byte ptr [counter],0
-            jne Yes
+    m2:
+        pop di
+        inc di
+        pop cx
+        loop m1
+        cmp byte ptr [counter],0
+        jne Yes
 
-        No:
-            lea dx,txtno
-            mov ah,9
-            int 21h
-            jmp quit
+    No:
+        lea dx,txtno
+        mov ah,9
+        int 21h
+        jmp quit
 
-        Yes:
-            lea dx,crlf
-            mov ah,9
-            int 21h
-            lea dx,txtyes
-            int 21h
-            mov al,counter
-            xor ah,ah
-            mov bx,300Ah
-            div bl
-            add bh,ah
-            xor ah,ah
-            div bl
-            mov dx,3030h
-            add dx,ax
-            mov ah,2
-            int 21h
-            mov dl,dh
-            int 21h
-            mov dl,bh
-            int 21h
+    Yes:
+        lea dx,crlf
+        mov ah,9
+        int 21h
+        lea dx,txtyes
+        int 21h
+        mov al,counter
+        xor ah,ah
+        mov bx,300Ah
+        div bl
+        add bh,ah
+        xor ah,ah
+        div bl
+        mov dx,3030h
+        add dx,ax
+        mov ah,2
+        int 21h
+        mov dl,dh
+        int 21h
+        mov dl,bh
+        int 21h
 
-        quit:
-            mov ah,8
-            int 21h
-            mov ax,4C00h
-            int 21h
+    quit:
+        mov ah,8
+        int 21h
+        mov ax,4C00h
+        int 21h
 code ends
 
 stack segment
