ff9d46ef942a5918b2d1fb69da372dfd6dabc6b3
Никита Фомин <defdXs@yandex.ru>
2024-09-15 14:06:44 +0300

    New cool navigation style

---
+++ csharp_language/ProrgrammingLanguages/FifthSemester/Project/Shield.App/Views/AlarmsTableViewPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using Shield.App.ViewModels;
+
+namespace Shield.App.Views;
+public sealed partial class AlarmsTableViewPage : Page
+{
+    public AlarmsTableViewViewModel ViewModel
+    {
+        get;
+    }
+
+    public AlarmsTableViewPage()
+    {
+        ViewModel = App.GetService<AlarmsTableViewViewModel>();
+        InitializeComponent();
+    }
+}
