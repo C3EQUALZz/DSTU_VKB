cf43e629afa9c62514e8801734eee8d696e016ff
matebookWindows <dan.kovalev2013@gmail.com>
2024-09-19 21:19:46 +0300

    Перенес первую лабораторную по теории информации

--- python_language/theory_of_information/models/first_laboratory/entropy.py
+++ python_language/theory_of_information/models/first_laboratory/entropy.py
@@ -1,32 +1,62 @@
+import math
 import re
-from pathlib import Path
-from collections import Counter
-from typing import Mapping, SupportsFloat, AnyStr, Final
 
+from collections import Counter
+from pathlib import Path
+from typing import Mapping, AnyStr, Final
 
 IGNORE_PATTERN: Final[AnyStr] = r'[@#$^&*{}[\]<>+=/\\|£№]'
 
 
-def get_character_probabilities(file_path: Path, ignore_pattern: re.Pattern[AnyStr] = None) -> Mapping[AnyStr, SupportsFloat]:
-    """
+class Model:
 
-    """
-    if not file_path.exists():
-        raise FileNotFoundError(f"Нет указанного файла, проверьте корректность {file_path}")
+    def create_histogram(self, file_path: Path, ignore_pattern):
+        probabilities = self.calculate_character_probabilities(file_path, ignore_pattern)
 
-    if ignore_pattern is None:
-        ignore_pattern = IGNORE_PATTERN
 
-    char_counts = Counter()
-    total_chars = 0
+    @staticmethod
+    def calculate_character_probabilities(
+            file_path: Path,
+            ignore_pattern: re.Pattern[AnyStr] = None
+    ) -> Mapping[AnyStr, float]:
 
-    with file_path.open('r', encoding='utf-8') as file:
-        for row in file:
-            filtered_text = re.sub(ignore_pattern, '', row)
-            char_counts.update(filtered_text)
-            total_chars += len(filtered_text)
+        """
+        Подсчет вероятности символов текста из файла.
+        :param file_path: Путь до txt файла с текстом.
+        :param ignore_pattern: Паттерн, который надо использовать для игнорирования текста
+        """
+        if not file_path.exists():
+            raise FileNotFoundError(f"Нет указанного файла, проверьте корректность {file_path}")
 
-    if total_chars == 0:
-        return {}
+        if file_path.suffix.lower() != '.txt':
+            raise ValueError(f"Поддерживается работа только с txt файлами")
 
-    return {char: count / total_chars for char, count in char_counts.items()}
+        if ignore_pattern is None:
+            ignore_pattern = IGNORE_PATTERN
+
+        char_counts = Counter()
+        total_chars = 0
+
+        with file_path.open('r', encoding='utf-8') as file:
+            for row in file:
+                filtered_text = re.sub(ignore_pattern, '', row)
+                char_counts.update(filtered_text)
+                total_chars += len(filtered_text)
+
+        if total_chars == 0:
+            return {}
+
+        return {char: count / total_chars for char, count in char_counts.items()}
+
+    @staticmethod
+    def calculate_entropy(
+            dictionary: Mapping[AnyStr, float]
+    ) -> float:
+        """
+        Получаем энтропию, зная вероятности каждого символа
+        :param dictionary: словарь с вероятностями появления каждого символа
+        """
+        probabilities = dictionary.values()
+        return -1 * sum(probability * math.log2(probability) for probability in probabilities)
+
+
