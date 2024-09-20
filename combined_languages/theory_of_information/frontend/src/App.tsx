753dde5d861521c042da183cc101efe9c5e65df1
matebookWindows <dan.kovalev2013@gmail.com>
2024-09-20 17:16:29 +0300

    Первая версия интерфейса

--- combined_languages/theory_of_information/frontend/src/App.tsx
+++ combined_languages/theory_of_information/frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import { useState } from "react";
-import { Button, Layout, theme } from "antd";
-import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
-import { Logo } from "./components/Logo"
-import { MenuList } from "./components/MenuList.tsx";
-import { ToggleThemeButton } from "./components/ToggleThemeButton.tsx";
+import {useState} from "react";
+import {Button, Layout, theme, Typography} from "antd";
+import {MenuUnfoldOutlined, MenuFoldOutlined, GithubOutlined} from "@ant-design/icons";
+import {Logo} from "./components/Logo"
+import {MenuList} from "./components/MenuList.tsx";
+import {ToggleThemeButton} from "./components/ToggleThemeButton.tsx";
 
-import { Route, Routes, BrowserRouter as Router} from "react-router-dom";
+import {Route, Routes, BrowserRouter as Router} from "react-router-dom";
 
 import {
     FirstLaboratory as FirstLaboratoryFifthSem,
@@ -21,40 +21,81 @@ import {
     FourthLaboratory as FourthLaboratorySixthSem
 } from "./pages/sixth-semester"
 
-const {Header, Sider, Content} = Layout;
+const {Header, Sider, Content, Footer} = Layout;
 
 export function App() {
     const [darkTheme, setDarkTheme] = useState<boolean>(true);
     const [collapsed, setCollapsed] = useState<boolean>(false);
 
-    const toggleTheme = () => { setDarkTheme(!darkTheme); }
-    const {token: { colorBgContainer }} = theme.useToken();
+    const toggleTheme = () => {
+        setDarkTheme(!darkTheme);
+    }
+    const {token: {colorBgContainer}} = theme.useToken();
 
     return (
         <>
             <Router>
                 <Layout>
-                    <Sider width={270} style={{height: '100vh'}} collapsed={collapsed} collapsible trigger={null} theme={darkTheme ? "dark" : "light"}>
-                        <Logo />
-                        <MenuList darkTheme={darkTheme} />
-                        <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme} />
+                    <Sider width={270} style={{height: '100vh'}} collapsed={collapsed} collapsible trigger={null}
+                           theme={darkTheme ? "dark" : "light"}>
+                        <Logo/>
+                        <MenuList darkTheme={darkTheme}/>
+                        <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme}/>
                     </Sider>
                     <Layout>
-                        <Header style={{padding: 0, background: colorBgContainer}}>
-                            <Button className="toggle" type="text" icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />} onClick={() => setCollapsed(!collapsed)} />
+                        <Header style={{
+                            padding: 0,
+                            background: darkTheme ? '#001529' : '#f0f2f5',
+                            color: darkTheme ? '#fff' : '#000'
+                        }}>
+                            <Button
+                                className="toggle"
+                                type="text"
+                                icon={collapsed ? <MenuUnfoldOutlined/> : <MenuFoldOutlined/>}
+                                onClick={() => setCollapsed(!collapsed)}
+                                style={{
+                                    padding: 0,
+                                    background: darkTheme ? '#001529' : '#f0f2f5',
+                                    color: darkTheme ? '#fff' : '#000'
+                                }}
+                            />
+                            <Typography.Text
+                                strong
+                                style={{
+                                    padding: 0,
+                                    background: darkTheme ? '#001529' : '#f0f2f5',
+                                    color: darkTheme ? '#fff' : '#000'
+                                }}>
+                                Лабораторные работы по предмету теория информации
+                            </Typography.Text>
                         </Header>
-                        <Content style={{ padding: '24px', background: colorBgContainer }}>
-                           <Routes>
-                               <Route path="/" element={<FirstLaboratoryFifthSem />}/>
-                               <Route path="/5-sem/2-lab" element={<SecondLaboratoryFifthSem />} />
-                               <Route path="/5-sem/3-lab" element={<ThirdLaboratoryFifthSem />} />
-                               <Route path="/5-sem/4-lab" element={<FourthLaboratoryFifthSem />} />
-                               <Route path="/6-sem/1-lab" element={<FirstLaboratorySixthSem />} />
-                               <Route path="/6-sem/2-lab" element={<SecondLaboratorySixthSem />} />
-                               <Route path="/6-sem/3-lab" element={<ThirdLaboratorySixthSem/>} />
-                               <Route path="/6-sem/4-lab" element={<FourthLaboratorySixthSem/>} />
+                        <Content style={{padding: '24px', background: colorBgContainer}}>
+                            <Routes>
+                                <Route path="/" element={<FirstLaboratoryFifthSem/>}/>
+                                <Route path="/5-sem/2-lab" element={<SecondLaboratoryFifthSem/>}/>
+                                <Route path="/5-sem/3-lab" element={<ThirdLaboratoryFifthSem/>}/>
+                                <Route path="/5-sem/4-lab" element={<FourthLaboratoryFifthSem/>}/>
+                                <Route path="/6-sem/1-lab" element={<FirstLaboratorySixthSem/>}/>
+                                <Route path="/6-sem/2-lab" element={<SecondLaboratorySixthSem/>}/>
+                                <Route path="/6-sem/3-lab" element={<ThirdLaboratorySixthSem/>}/>
+                                <Route path="/6-sem/4-lab" element={<FourthLaboratorySixthSem/>}/>
                             </Routes>
                         </Content>
+                        <Footer style={{
+                            textAlign: 'center',
+                            background: darkTheme ? '#001529' : '#f0f2f5',
+                            color: darkTheme ? '#fff' : '#000'
+                        }}>
+                            <Typography.Text strong style={{
+                                padding: 0,
+                                background: darkTheme ? '#001529' : '#f0f2f5',
+                                color: darkTheme ? '#fff' : '#000'
+                            }}>Выполнил: Ковалев Данил ВКБ32</Typography.Text>
+                            <a href="https://github.com/ваш_профиль" target="_blank" rel="noopener noreferrer"
+                               style={{marginLeft: '10px'}}>
+                                <GithubOutlined/>
+                            </a>
+                        </Footer>
                     </Layout>
                 </Layout>
             </Router>
