b1d71b81d621ff53c61fa14d08f825cc6a504b64
c3equalz <dan.kovalev2013@gmail.com>
2024-11-09 23:11:00 +0300

    Вынес логику регистрации в родительский класс, исправил типизацию

--- combined_languages/theory_of_information/backend/fifth_semestr/fourth_laboratory/utils/factories/inverse_matrix.py
+++ combined_languages/theory_of_information/backend/fifth_semestr/fourth_laboratory/utils/factories/inverse_matrix.py
@@ -1,36 +1,21 @@
-from importlib import import_module
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal
 
 import numpy as np
 
 from combined_languages.theory_of_information.backend.fifth_semestr.fourth_laboratory.models.base import Matrix
 from combined_languages.theory_of_information.backend.fifth_semestr.fourth_laboratory.utils.factories.base import \
-    Factory
+    RegistryFactory
 
 if TYPE_CHECKING:
     from combined_languages.theory_of_information.backend.fifth_semestr.fourth_laboratory.models.base import \
         SystematicMatrix
 
-PATH_TO_MODELS = "combined_languages.theory_of_information.backend.fifth_semestr.fourth_laboratory.models."
 
-
-class InverseMatrixFactory(Factory):
-    _registry = {}
-
-    # NOTE: я не придумал как сделать нормально без динамического импорта.
-    # У Python есть проблема с циклическими импортами. В случае бы GSystematic и Hsystematic просто импортировали бы друг друга, а это плохо
-    @classmethod
-    def register(cls, matrix_type: str, matrix_class_name: str) -> None:
-        module_name, class_name = (PATH_TO_MODELS + matrix_class_name).rsplit('.', 1)
-        matrix_class = getattr(import_module(module_name), class_name)
-        cls._registry[matrix_type] = matrix_class
+class InverseMatrixFactory(RegistryFactory):
 
     @classmethod
-    def create(cls, matrix: Matrix, matrix_type: str) -> "SystematicMatrix":
+    def create(cls, matrix: Matrix, matrix_type: Literal["G", "H"]) -> "SystematicMatrix":
         matrix_cls = cls._registry.get(matrix_type)
-
-        if not matrix_cls:
-            raise ValueError(f"Unknown matrix type: {matrix_type}")
 
         n = matrix.shape[1]
         k = matrix.shape[0]
@@ -45,3 +30,5 @@ class InverseMatrixFactory(Factory):
             eye_matrix = np.eye(n - k, dtype=required_columns.dtype)
             new_matrix = np.hstack((eye_matrix, required_columns)).tolist()
             return matrix_cls(new_matrix)
+
+        raise ValueError(f"Неизвестный тип матрицы: {matrix_type}")
