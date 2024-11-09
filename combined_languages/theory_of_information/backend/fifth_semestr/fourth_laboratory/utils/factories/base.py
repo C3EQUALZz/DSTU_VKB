7ae674775b22f44fe856c820603e0323ffbfef2d
c3equalz <dan.kovalev2013@gmail.com>
2024-11-09 23:09:13 +0300

    Добавил фабрику - регистратор, который позволяет создавать классы с помощью "рефлексии"

--- combined_languages/theory_of_information/backend/fifth_semestr/fourth_laboratory/utils/factories/base.py
+++ combined_languages/theory_of_information/backend/fifth_semestr/fourth_laboratory/utils/factories/base.py
@@ -1,5 +1,8 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from importlib import import_module
+from typing import Any, Final
+
+PATH_TO_MODELS: Final = "combined_languages.theory_of_information.backend.fifth_semestr.fourth_laboratory.models."
 
 
 class Factory(ABC):
@@ -7,3 +10,17 @@ class Factory(ABC):
     @abstractmethod
     def create(cls, matrix, matrix_type: str) -> Any:
         ...
+
+
+class RegistryFactory(Factory, ABC):
+    _registry = {}
+
+    # NOTE: я не придумал как сделать нормально без динамического импорта.
+    # У Python есть проблема с циклическими импортами.
+    # В случае бы GSystematic и Hsystematic просто импортировали бы друг друга, а это плохо
+    @classmethod
+    def register(cls, matrix_type: str, matrix_class_name: str) -> None:
+        module_name, class_name = (PATH_TO_MODELS + matrix_class_name).rsplit('.', 1)
+        matrix_class = getattr(import_module(module_name), class_name)
+        cls._registry[matrix_type] = matrix_class
+
