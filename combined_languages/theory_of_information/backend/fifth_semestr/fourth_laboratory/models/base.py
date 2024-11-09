eeb93e4cf8752f690fb0b4f3fde55a505c001e20
c3equalz <dan.kovalev2013@gmail.com>
2024-11-09 22:20:06 +0300

    Добавил здесь абстрактные классы, пометил, что не должно быть их экземпляров.

--- combined_languages/theory_of_information/backend/fifth_semestr/fourth_laboratory/models/base.py
+++ combined_languages/theory_of_information/backend/fifth_semestr/fourth_laboratory/models/base.py
@@ -1,38 +1,15 @@
-from typing import List
-
+from typing import List, Tuple, AnyStr
+from abc import ABC, abstractmethod
 import numpy as np
 
 
-class Matrix:
+class Matrix(ABC):
     def __init__(self, matrix: List[List[int]]) -> None:
         self.matrix = np.array(matrix)
 
-    def to_systematic_form(self) -> "SystematicMatrix":
-        raise NotImplementedError
-
-    def transpose(self) -> "Matrix":
-        return Matrix(self.matrix.transpose().tolist())
-
-    def __len__(self) -> int:
-        return len(self.matrix)
-
     @property
-    def shape(self) -> tuple[int, ...]:
+    def shape(self) -> Tuple[int, ...]:
         return self.matrix.shape
-
-    def __getitem__(self, key) -> "Matrix":
-        return Matrix(self.matrix[key].tolist())
-
-    def __str__(self) -> str:
-        return "\n".join(str(row) for row in self.matrix)
-
-
-class SystematicMatrix(Matrix):
-    def __init__(self, matrix: List[List[int]]) -> None:
-        super().__init__(matrix)
-
-    def find_another_type_matrix(self) -> "SystematicMatrix":
-        raise NotImplemented()
 
     @property
     def n(self) -> int:
@@ -41,3 +18,27 @@ class SystematicMatrix(Matrix):
     @property
     def k(self) -> int:
         return self.matrix.shape[0]
+
+    def __len__(self) -> int:
+        return len(self.matrix)
+
+    def __getitem__(self, key) -> "Matrix":
+        return Matrix(self.matrix[key].tolist())
+
+    def __str__(self) -> AnyStr:
+        return "\n".join(str(row) for row in self.matrix)
+
+    def to_systematic_form(self) -> "SystematicMatrix":
+        raise NotImplementedError
+
+    def transpose(self) -> "Matrix":
+        return Matrix(self.matrix.transpose().tolist())
+
+
+class SystematicMatrix(Matrix, ABC):
+    def __init__(self, matrix: List[List[int]]) -> None:
+        super().__init__(matrix)
+
+    @abstractmethod
+    def find_another_type_matrix(self) -> "SystematicMatrix":
+        raise NotImplemented()
