4b02cc4c038f0d2e8681acbcdbb7ccb2d57c5190
matebookWindows <dan.kovalev2013@gmail.com>
2024-11-10 11:20:35 +0300

    Добавил в отслеживание порождающие и проверочные матрицы

--- combined_languages/theory_of_information/backend/fifth_semestr/fourth_laboratory/models/generator_matrix.py
+++ combined_languages/theory_of_information/backend/fifth_semestr/fourth_laboratory/models/generator_matrix.py
@@ -5,16 +5,18 @@ from combined_languages.theory_of_information.backend.fifth_semestr.fourth_labor
     GSystematicMatrix
 from combined_languages.theory_of_information.backend.fifth_semestr.fourth_laboratory.utils.factories.systematic import \
     SystematicMatrixFactory
+from combined_languages.theory_of_information.backend.fifth_semestr.fourth_laboratory.utils.registry import Registry
 
 
 class GMatrix(Matrix):
     def __init__(self, matrix: List[List[int]]) -> None:
         super().__init__(matrix)
+        Registry.log(f"{self.__class__.__name__}", "\n".join(map(str, matrix)))
         SystematicMatrixFactory.register("G", "generator_systematic_matrix.GSystematicMatrix")
 
     def to_systematic_form(self) -> "GSystematicMatrix":
         """
         Находит систематический вид матрицы, удаляя столбцы с единственным значением 1.
-        :returns: Модифицированная проверочная матрица в систематическом виде.
+        :returns: Модифицированная порождающая матрица в систематическом виде.
         """
         return cast(GSystematicMatrix, SystematicMatrixFactory.create(self, "G"))
--- combined_languages/theory_of_information/backend/fifth_semestr/fourth_laboratory/models/verification_matrix.py
+++ combined_languages/theory_of_information/backend/fifth_semestr/fourth_laboratory/models/verification_matrix.py
@@ -5,18 +5,18 @@ from combined_languages.theory_of_information.backend.fifth_semestr.fourth_labor
     HSystematicMatrix
 from combined_languages.theory_of_information.backend.fifth_semestr.fourth_laboratory.utils.factories.systematic import \
     SystematicMatrixFactory
+from combined_languages.theory_of_information.backend.fifth_semestr.fourth_laboratory.utils.registry import Registry
 
 
 class HMatrix(Matrix):
     def __init__(self, matrix: List[List[int]]) -> None:
         super().__init__(matrix)
+        Registry.log(f"{self.__class__.__name__}", "\n".join(map(str, matrix)))
         SystematicMatrixFactory.register("H", "verification_systematic_matrix.HSystematicMatrix")
 
     def to_systematic_form(self) -> "HSystematicMatrix":
         """
         Находит систематический вид матрицы, удаляя столбцы с единственным значением 1.
-
-        Returns:
-            NDArray[np.int_]: Модифицированная проверочная матрица в систематическом виде.
+        :returns: Модифицированная проверочная матрица в систематическом виде.
         """
         return cast(HSystematicMatrix, SystematicMatrixFactory.create(self, "H"))
