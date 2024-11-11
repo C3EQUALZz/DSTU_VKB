4c083738c29e31784a6a030da16b84ac32304ed6
c3equalz <dan.kovalev2013@gmail.com>
2024-11-11 19:01:20 +0300

    Набросал пока всякое

--- python_language/machine_learning/first_semester/second_laboratory/seventh_question.py
+++ python_language/machine_learning/first_semester/second_laboratory/seventh_question.py
@@ -1,5 +1,11 @@
 """
 Напишите функцию, которая будет производить поиск по значению и выдавать ключ.
+
+Тестовые данные:
+- [
+    {"1": "first_value", "2": "second_value", "3": "third_value"}
+    first_value
+]
 """
 import ast
 
@@ -15,7 +21,6 @@ def search_key(value: str, dictionary: dict[str, str]) -> str:
 def main() -> None:
     user_input = input("Вводите словарь по следующему правилу {key: value, key: value, ...}: ")
     dictionary: dict[str, str] = ast.literal_eval(user_input)
-    print(dictionary)
     value = input("Введите значение ")
     print(search_key(value, dictionary))
 
