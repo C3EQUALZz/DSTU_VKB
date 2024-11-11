4f69191545ef51eaad32a914d5e110c57fd0c07f
matebookWindows <dan.kovalev2013@gmail.com>
2024-11-11 12:43:45 +0300

    Убрал ограничение по CORS

--- combined_languages/theory_of_information/backend/main.py
+++ combined_languages/theory_of_information/backend/main.py
@@ -14,15 +14,9 @@ app = FastAPI(
     debug=True,
 )
 
-
-origins = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
-
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "OPTIONS", "DELETE", "PATCH", "PUT"],
     allow_headers=["Content-Type", "Set-Cookie", "Access-Control-Allow-Headers",
