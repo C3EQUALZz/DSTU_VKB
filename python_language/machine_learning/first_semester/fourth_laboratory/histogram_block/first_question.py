5f077517d451a3a0877a57f997e7eb31da66f7a1
c3equalz <dan.kovalev2013@gmail.com>
2024-11-13 21:47:28 +0300

    Сделал 1 задание по гистограммам в 4 лабе

---
+++ python_language/machine_learning/first_semester/fourth_laboratory/histogram_block/first_question.py
@@ -0,0 +1,27 @@
+"""
+1. Нарисуйте горизонтальную гистограмму с помощью Matplotlib
+"""
+import matplotlib.pyplot as plt
+from dataclasses import dataclass, field
+from random import randint, choice
+import string
+
+
+@dataclass
+class Column:
+    data: int = field(default_factory=lambda: randint(0, 100))
+    name: str = field(default_factory=lambda: f"{choice(string.ascii_uppercase)}")
+
+
+def draw_horizontal_histograms(*args: Column) -> None:
+    for column in args:
+        plt.barh(column.name, column.data)
+    plt.show()
+
+
+def main() -> None:
+    draw_horizontal_histograms(Column(), Column(), Column(), Column())
+
+
+if __name__ == '__main__':
+    main()
