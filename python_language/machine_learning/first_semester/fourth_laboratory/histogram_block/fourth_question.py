84d7d675ab1575d1ded737e1cab9e3c125c93f4f
c3equalz <dan.kovalev2013@gmail.com>
2024-11-14 19:37:34 +0300

    Брррр

---
+++ python_language/machine_learning/first_semester/fourth_laboratory/histogram_block/fourth_question.py
@@ -0,0 +1,15 @@
+"""
+4. Как аннотировать столбцы в сгруппированном столбчатом графике на Python?
+"""
+
+
+def draw_grouped_histogram_and_annotate_each_column() -> None:
+    ...
+
+
+def main() -> None:
+    ...
+
+
+if __name__ == '__main__':
+    main()
