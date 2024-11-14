29a9bdcc0809ca46849380f24bd8ceee4f73d3a8
c3equalz <dan.kovalev2013@gmail.com>
2024-11-14 23:37:45 +0300

    Закончил с гистограммами

--- python_language/machine_learning/first_semester/fourth_laboratory/histogram_block/fifth_question.py
+++ python_language/machine_learning/first_semester/fourth_laboratory/histogram_block/fifth_question.py
@@ -1,15 +1,29 @@
 """
-5. Изобразите столбчатую диаграмму колонки «Класс жилья». Расположите в порядке убывания размеры столбцов. В ответ запишите нужную последовательность цифр, например 1234 (начальные данные  из ноута "Создание наглядных графиков")
+5. Изобразите столбчатую диаграмму колонки «Класс жилья». Расположите в порядке убывания размеры столбцов.
+В ответ запишите нужную последовательность цифр, например 1234 (начальные данные из ноута "Создание наглядных графиков")
+
 Данные можно взять из ноута "Создание наглядных графиков"
 """
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
-def draw_histogram() -> None:
-    ...
+def draw_histogram_by_param(data: pd.Series) -> None:
+    plt.bar(data.index, data.values)
+    plt.xticks(rotation=30, ha='right')
+    plt.xlabel('Класс жилья')
+    plt.ylabel('Число объявлений')
+    plt.title('Распределение объявлений по классу жилья')
+    plt.show()
+
+    print('Порядок убывания размеров столбцов:', ''.join(map(str, data.index)))
 
 
 def main() -> None:
-    ...
+    dataset = pd.read_csv('../housing_market_dataset.csv')
+    type_counts = dataset['Класс жилья'].value_counts()
+    type_counts = type_counts.sort_values(ascending=False)
+    draw_histogram_by_param(type_counts)
 
 
 if __name__ == '__main__':
