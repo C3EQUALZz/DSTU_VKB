87f38a3cbd6c406a0b3f17c03638647b8e0eb3e1
c3equalz <dan.kovalev2013@gmail.com>
2024-11-15 19:25:38 +0300

    6 задание 4 лабы по общим настройкам

--- python_language/machine_learning/first_semester/fourth_laboratory/general_settings/sixth_question.py
+++ python_language/machine_learning/first_semester/fourth_laboratory/general_settings/sixth_question.py
@@ -1,3 +1,35 @@
 """
 6. Как сгенерировать случайный цвет для графика Matplotlib в Python?
 """
+from dataclasses import dataclass, field
+from random import randint, choice
+
+import matplotlib.pyplot as plt
+
+
+@dataclass
+class Line:
+    level: int = field(default_factory=lambda: randint(1, 10))
+    color: str = field(default_factory=lambda: choice(('r', 'g', 'b')))
+    line_style: str = field(default_factory=lambda: choice(('--', ':', '-')))
+    line_width: int = field(default_factory=lambda: randint(1, 5))
+
+
+def draw_lines_and_change_line_colors(*args: Line) -> None:
+    for line in args:
+        plt.axvline(x=line.level, color=line.color, linestyle=line.line_style, linewidth=line.line_width)
+
+    plt.gca().set_facecolor('lightgrey')
+    plt.show()
+
+
+def main() -> None:
+    draw_lines_and_change_line_colors(
+        Line(),
+        Line(),
+        Line()
+    )
+
+
+if __name__ == '__main__':
+    main()
