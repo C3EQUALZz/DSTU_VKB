4e47ade4bc9799da96f18bc2d116113ab80f3451
c3equalz <dan.kovalev2013@gmail.com>
2024-11-15 18:20:36 +0300

    1 задание 4 лабы по общим настройкам

--- python_language/machine_learning/first_semester/fourth_laboratory/general_settings/first_question.py
+++ python_language/machine_learning/first_semester/fourth_laboratory/general_settings/first_question.py
@@ -1,3 +1,28 @@
 """
 1. Как изменить прозрачность графика в Matplotlib с помощью Python?
 """
+from dataclasses import dataclass, field
+from random import randint, choice
+
+import matplotlib.pyplot as plt
+
+
+@dataclass
+class Line:
+    level: int = field(default_factory=lambda: randint(1, 10))
+    color: str = field(default_factory=lambda: choice(('r', 'g', 'b')))
+    line_style: str = field(default_factory=lambda: choice(('--', ':', '-')))
+    line_width: int = field(default_factory=lambda: randint(1, 5))
+
+
+def draw_vertical_line_and_change_transparency(line: Line) -> None:
+    plt.axvline(x=line.level, color=line.color, linestyle=line.line_style, linewidth=line.line_width, alpha=0.1)
+    plt.show()
+
+
+def main() -> None:
+    draw_vertical_line_and_change_transparency(Line())
+
+
+if __name__ == '__main__':
+    main()
