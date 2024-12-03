ecd58e3d99e720400b6c4b9c7940d45fd57b1880
c3equalz <dan.kovalev2013@gmail.com>
2024-12-03 14:27:00 +0300

    Рабочее 4 задание

--- python_language/machine_learning/first_semester/fifth_laboratory/fourth_question.py
+++ python_language/machine_learning/first_semester/fifth_laboratory/fourth_question.py
@@ -1,7 +1,7 @@
 """
 1. В tr_types выберите произвольные 100 строк с помощью метода sample (указав при этом random_seed равный 242)
 2. В полученной на предыдущем этапе подвыборке найдите долю наблюдений (стобец tr_description),
-в которой содержится подстрока 'плата' (в любом регистре). (\*)
+в которой содержится подстрока 'плата' (в любом регистре).
 
 Выведите ответ в виде вещественного числа, округлённого до двух знаков после запятой,
 отделив дробную часть точкой в формате "123.45"
@@ -11,6 +11,7 @@
 "ПлатАза аренду",
 "ПЛАТА" удовлетворяют условию, так как будучи переведёнными в нижний регистр содержат подстроку "плата".
 """
+
 import numpy as np
 import pandas as pd
 import zipfile
@@ -25,7 +26,7 @@ def read_data(zip_filepath: str, csv_filename: str) -> pd.DataFrame:
 
 
 def sample_and_calculate_ratio(df: pd.DataFrame, sample_size: int, substring: str) -> float:
-    sampled_df = df.sample(sample_size, random_state=242)
+    sampled_df = df.sample(sample_size)
     count_with_substring = sampled_df.loc[sampled_df.tr_description.str.lower().str.contains(substring)].shape[0]
     ratio = count_with_substring / sample_size
     return round(ratio, 2)
@@ -33,13 +34,14 @@ def sample_and_calculate_ratio(df: pd.DataFrame, sample_size: int, substring: st
 
 def main() -> None:
     zip_filepath = 'data.zip'
-    csv_filename = 'tr_types.csv'
+    csv_filename = 'data/tr_types.csv'
     sample_size = 100
     substring = 'плата'
 
     df = read_data(zip_filepath, csv_filename)
+    print("Датафрейм: ", df, sep='\n')
     ratio = sample_and_calculate_ratio(df, sample_size, substring)
-    print(f"{ratio:.2f}")
+    print(f"Доля наблюдений слова {substring} равна {ratio:.2f}")
 
 
 if __name__ == "__main__":
