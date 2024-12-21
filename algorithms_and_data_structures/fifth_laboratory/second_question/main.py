0debc8fdccd0f873d37b41f3e54e61f11e5b78e1
c3equalz <dan.kovalev2013@gmail.com>
2024-12-21 19:57:34 +0300

    готовые реализации лаб по алгоритмам

--- algorithms_and_data_structures/fifth_laboratory/second_question/main.py
+++ algorithms_and_data_structures/fifth_laboratory/second_question/main.py
@@ -7,6 +7,10 @@ from algorithms_and_data_structures.first_laboratory.first_question.main import 
     main as first_laboratory_first_question_main
 from algorithms_and_data_structures.first_laboratory.second_question.main import \
     main as first_laboratory_second_question_main
+from algorithms_and_data_structures.fourth_laboratory.first_question.main import \
+    main as fourth_laboratory_first_question_main
+from algorithms_and_data_structures.fourth_laboratory.second_question.main import \
+    main as fourth_laboratory_second_question_main
 from algorithms_and_data_structures.second_laboratory.first_question.main import \
     main as second_laboratory_first_question_main
 from algorithms_and_data_structures.second_laboratory.second_question.main import \
@@ -32,7 +36,10 @@ CHOICES: Final[Dict] = {
         "2": third_laboratory_second_question_main,
         "3": third_laboratory_third_question_main,
     },
-    "4": {},
+    "4": {
+        "1": fourth_laboratory_first_question_main,
+        "2": fourth_laboratory_second_question_main
+    },
     "5": {
         "1": fifth_laboratory_first_question_main
     }
