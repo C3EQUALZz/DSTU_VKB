e0ee582130ddde0fda96557819787d43f05b0961
c3equalz <dan.kovalev2013@gmail.com>
2024-12-22 22:56:35 +0300

    Переношу машинное обучение

---
+++ machine_learning/first_semester/second_laboratory/fifth_question.py
@@ -0,0 +1,26 @@
+"""
+Задан список с числами.
+Напишите функцию, которая меняет местами наибольший и наименьший элемент и возвращает новый список.
+Тестовые данные:
+- 1 2 3 4 -10 6 7 10 11 15 123123123
+"""
+
+
+def change_max_min(numbers: list[int]) -> list[int]:
+    result = numbers.copy()
+    i, j = numbers.index(max(numbers)), numbers.index(min(numbers))
+    result[i], result[j] = result[j], result[i]
+    return result
+
+
+def main() -> None:
+    user_input = input("Вводите числа через пробел: ").strip().split()
+
+    if all(x.isdigit() or (x[0] == "-" and x[1:].isdigit()) for x in user_input):
+        print(change_max_min(list(map(int, user_input))))
+    else:
+        print("Вы ввели не числа")
+
+
+if __name__ == '__main__':
+    main()
