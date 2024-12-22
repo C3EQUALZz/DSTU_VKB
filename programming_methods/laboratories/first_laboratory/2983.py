e6f7d0ad82807cbc11a43d189b39e1cbf4887173
c3equalz <dan.kovalev2013@gmail.com>
2024-12-23 00:10:00 +0300

    Делаю методы программирования

--- programming_methods/laboratories/first_laboratory/2983.py
+++ programming_methods/laboratories/first_laboratory/2983.py
@@ -16,3 +16,50 @@
 следует также зафиксировать ошибку.
 От вас требуется либо указать максимальное количество бочек, которые одновременно пребывали на барже либо зафиксировать ошибку.
 """
+from collections import deque
+from typing import List, Deque
+
+
+def main() -> None:
+    # Чтение входных данных
+    request: List[str] = input().split()
+    docks: int = int(request[0])
+    cells: int = int(request[1])
+    max_len: int = int(request[2])
+    max_tanks: int = 0
+    current_tanks: int = 0
+    error: bool = False
+
+    # Инициализация отсеков
+    cell: List[Deque[str]] = [deque() for _ in range(cells)]
+
+    # Обработка действий в доках
+    for _ in range(docks):
+        action: List[str] = input().split()
+        if action[0] == "+":
+            # Загрузка бочки
+            cell_index: int = int(action[1]) - 1
+            fuel_type: str = action[2]
+            cell[cell_index].append(fuel_type)
+            current_tanks += 1
+
+            if current_tanks > max_len:
+                error = True
+                break
+            max_tanks = max(max_tanks, current_tanks)
+        else:
+            # Выгрузка бочки
+            cell_index: int = int(action[1]) - 1
+            fuel_type: str = action[2]
+
+            if not cell[cell_index] or cell[cell_index].pop() != fuel_type:
+                error = True
+                break
+            current_tanks -= 1
+
+    # Проверка на ошибки и вывод результата
+    print("Error" if current_tanks > 0 or error else max_tanks)
+
+
+if __name__ == "__main__":
+    main()
