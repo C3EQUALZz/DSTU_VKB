043d52b4d5799cb097108234211c3ebc02b71e45
matebookWindows <dan.kovalev2013@gmail.com>
2024-12-27 16:46:00 +0300

    набросал ещё вещей по поводу аутентификации

--- database_management_systems/8/backend/app/application/api/auth/handlers.py
+++ database_management_systems/8/backend/app/application/api/auth/handlers.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Annotated
 
 from authx import TokenPayload, AuthX
@@ -8,13 +9,14 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from starlette import status
 from starlette.requests import Request
 
-from app.application.api.auth.schemas import TokenRequest
+from app.application.api.auth.schemas import TokenRequest, UserSchemeResponse
 from app.core.types.handlers import EventHandlerMapping, CommandHandlerMapping
 from app.domain.entities.user import UserEntity
 from app.exceptions import ApplicationException
 from app.infrastructure.uow.users.base import UsersUnitOfWork
 from app.logic.bootstrap import Bootstrap
 from app.logic.commands.auth import VerifyUserCredentialsCommand
+from app.logic.commands.users import GetUserByIdCommand
 from app.logic.message_bus import MessageBus
 
 router = APIRouter(
@@ -23,7 +25,9 @@ router = APIRouter(
     route_class=DishkaRoute
 )
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/auth/login/")
+
+logger = logging.getLogger(__name__)
 
 
 @router.post(
@@ -54,16 +58,48 @@ async def login(
         )
 
     except ApplicationException as e:
+        logger.error(e)
         raise HTTPException(status_code=e.status, detail=e.message, headers={"WWW-Authenticate": "Bearer"})
 
 
 @router.post(
-    "/refresh/"
+    "/refresh/",
+    response_model_exclude_none=True
 )
 async def refresh(
         security: FromDishka[AuthX],
         request: Request
-):
+) -> TokenRequest:
     refresh_payload: TokenPayload = await security.refresh_token_required(request)
     access_token = security.create_access_token(refresh_payload.sub)
-    return {"access_token": access_token}
+    return TokenRequest(access_token=access_token)
+
+
+@router.get(
+    '/me/',
+    dependencies=[Depends(oauth2_scheme)]
+)
+async def get_me(
+        security: FromDishka[AuthX],
+        request: Request,
+        uow: FromDishka[UsersUnitOfWork],
+        events: FromDishka[EventHandlerMapping],
+        commands: FromDishka[CommandHandlerMapping]
+) -> UserSchemeResponse:
+    try:
+        access_token: TokenPayload = await security.access_token_required(request)
+
+        bootstrap: Bootstrap = Bootstrap(
+            uow=uow,
+            events_handlers_for_injection=events,
+            commands_handlers_for_injection=commands
+        )
+
+        messagebus: MessageBus = await bootstrap.get_messagebus()
+        await messagebus.handle(GetUserByIdCommand(access_token.sub))
+
+        return UserSchemeResponse.from_entity(messagebus.command_result)
+
+    except ApplicationException as e:
+        logger.error(e)
+        raise HTTPException(status_code=e.status, detail=e.message, headers={"WWW-Authenticate": "Bearer"})
--- database_management_systems/8/backend/app/application/api/auth/schemas.py
+++ database_management_systems/8/backend/app/application/api/auth/schemas.py
@@ -1,5 +1,8 @@
 from pydantic import BaseModel, EmailStr
-from typing import Optional
+from typing import Optional, Self
+
+from app.domain.entities.user import UserEntity
+
 
 class TokenRequest(BaseModel):
     access_token: str
@@ -7,25 +10,12 @@ class TokenRequest(BaseModel):
     token_type: str = "bearer"
 
 
-class UserLoginSchemaRequest(BaseModel):
-    email: EmailStr
-    password: str
-
-
-class UserLoginSchemaResponse(BaseModel):
-    access_token: str
-    refresh_token: str
-    email: EmailStr
+class UserSchemeResponse(BaseModel):
+    name: str
+    email: str
     oid: str
 
+    @classmethod
+    def from_entity(cls, entity: UserEntity) -> Self:
+        return cls(name=entity.name.as_generic_type(), email=entity.email.as_generic_type(), oid=entity.oid)
 
-class UserRegistrationSchemaRequest(BaseModel):
-    name: str
-    email: EmailStr
-    password: str
-
-
-class UserRegistrationSchemaResponse(BaseModel):
-    name: str
-    email: EmailStr
-    oid: str
