72ac94c14069d5c92691534d8d0c530b7c216796
c3equalz <dan.kovalev2013@gmail.com>
2025-01-10 19:45:15 +0300

    Исправил 4 задачу

--- programming_methods/laboratories/2/4.py
+++ programming_methods/laboratories/2/4.py
@@ -25,45 +25,59 @@
 Гарантируется, что входные данные соответствуют реальному турниру, то есть хотя бы одна таблица,
 соответствующая входным данным, может быть построена. Если таких таблиц несколько, выведите любую из них.
 """
-from typing import List
+from typing import List, Iterable
 
 
-def restore_tournament_table(num_teams: int, points: List[int]) -> List[List[int]]:
+def restore_tournament_table(num_teams: int, points: List[int]) -> Iterable[Iterable[int]]:
+    """
+    Здесь работает принцип динамического программирования.
+    У нас изначально, есть очки команд. Мы их сортируем, привязывая к индексам.
+
+    Теперь мы проходимся по каждой возможной паре команд и восстанавливаем таблицу.
+    Если у команды было 0 очков, то она скорее всего всем проиграла,
+    поэтому мы для второго элемента в паре ставим 2 в таблице, вычитая сразу 2 из очков.
+    Если уже позиция в таблице была заполнена, то делаем пропуск.
+    В ином случае, когда ничья, то ставим по 1 баллу каждой команде в паре.
+
+    :param num_teams: Количество команд.
+    :param points: Количество очков, набранных командами, занявшими первое, второе и т.п места.
+    :returns: Турнирная таблица.
+    """
     # Инициализация таблицы результатов
-    results = [[0] * num_teams for _ in range(num_teams)]
+    tournament_table: List[List[int]] = [[0] * num_teams for _ in range(num_teams)]
 
     # Индексы команд
-    team_indices = list(range(num_teams))
+    team_indices = range(num_teams)
 
     for i in range(num_teams - 1):
         # Сортируем команды по очкам
         sorted_points, sorted_indices = zip(*sorted(zip(points, team_indices)))
-        sorted_points = list(sorted_points)
-        sorted_indices = list(sorted_indices)
+        sorted_points: List[int] = list(sorted_points)
+        sorted_indices: List[int] = list(sorted_indices)
 
         for j in range(i + 1, num_teams):
-            if results[sorted_indices[i]][sorted_indices[j]] > 0:
+            if tournament_table[sorted_indices[i]][sorted_indices[j]] > 0:
                 continue
             elif sorted_points[i] > 0:
                 # Ничья
-                results[sorted_indices[i]][sorted_indices[j]] = 1
-                results[sorted_indices[j]][sorted_indices[i]] = 1
+                tournament_table[sorted_indices[i]][sorted_indices[j]] = 1
+                tournament_table[sorted_indices[j]][sorted_indices[i]] = 1
                 sorted_points[i] -= 1
                 points[sorted_indices[j]] -= 1
             elif sorted_points[i] == 0:
                 # Поражение
-                results[sorted_indices[j]][sorted_indices[i]] = 2
+                tournament_table[sorted_indices[j]][sorted_indices[i]] = 2
                 points[sorted_indices[j]] -= 2
         points[sorted_indices[i]] = -i - 1
 
-    return results
+    return tournament_table
 
 
-def main():
-    num_teams = int(input())
-    points = list(map(int, input().split()))
+def main() -> None:
+    num_teams: int = int(input())
+    points: List[int] = list(map(int, input().split()))
 
-    tournament_table = restore_tournament_table(num_teams, points)
+    tournament_table: Iterable[Iterable[int]] = restore_tournament_table(num_teams, points)
 
     for row in tournament_table:
         print(*row)
