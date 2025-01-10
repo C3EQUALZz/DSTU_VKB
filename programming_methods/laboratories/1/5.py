758a8416194e068d9c5219ebb180556643742352
c3equalz <dan.kovalev2013@gmail.com>
2025-01-10 10:41:20 +0300

    Отрефакторил 5 задание

--- programming_methods/laboratories/1/5.py
+++ programming_methods/laboratories/1/5.py
@@ -8,29 +8,38 @@
 Если A и B – правильные последовательности, то последовательность AB – правильная.
 """
 from collections import deque
+from typing import Dict
 
 
-def is_balanced(sequence: str) -> str:
-    """Проверяет, является ли последовательность скобок правильной."""
-    stack = deque()
-    matching_brackets = {')': '(', '}': '{', ']': '['}
+def is_balanced(sequence: str) -> bool:
+    """
+    Проверяет, является ли последовательность скобок правильной.
+    Опять-таки классическая задача на стек, но в Python нет готового стека, поэтому я буду пользоваться деком.
+    В словаре хранятся всевозможные пары, которые являются корректными.
+
+    Если у нас текущая скобочка является открывающей, то мы добавляем в стек, в ином случае проверяем,
+    что в стеке последнее.
+
+    :param sequence: Последовательность скобочек из условия задания.
+    :returns: True, если последовательность удовлетворяет условию задачи, в ином случае False
+    """
+    stack: deque[str] = deque()
+    matching_brackets: Dict[str, str] = {')': '(', '}': '{', ']': '['}
 
     for char in sequence:
-        if char in matching_brackets.values():  # Открывающая скобка
+        if char in matching_brackets.values():
             stack.append(char)
-        elif char in matching_brackets.keys():  # Закрывающая скобка
-            if stack and stack[-1] == matching_brackets[char]:
-                stack.pop()
-            else:
-                return 'no'
+        elif char in matching_brackets:
+            if not (stack and stack[-1] == matching_brackets[char]):
+                return False
+            stack.pop()
 
-    return 'yes' if not stack else 'no'
+    return not bool(stack)
 
 
 def main() -> None:
     sequence = input()
-    result = is_balanced(sequence)
-    print(result)
+    print("yes" if is_balanced(sequence) else "no")
 
 
 if __name__ == "__main__":
