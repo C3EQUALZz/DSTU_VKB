80dbce967b8559ed42b7b8249f46a4af35ecdf83
matebookWindows <dan.kovalev2013@gmail.com>
2025-01-10 15:43:36 +0300

    Улучшил 10

--- programming_methods/laboratories/1/10.py
+++ programming_methods/laboratories/1/10.py
@@ -27,12 +27,23 @@
 Если на протяжении 106 ходов игра не заканчивается, программа должна вывести слово botva.
 """
 from collections import deque
-from typing import Union, Tuple
+from typing import Iterable, Dict, Sequence
 
 
-def play_game(first_deck: deque[str], second_deck: deque[str]) -> Union[str, Tuple[str, int]]:
-    rounds = 0
-    max_rounds = 1_000_000
+def is_first_player_winner(card1: int, card2: int) -> bool:
+    # Определяем победителя по правилам игры
+    return (card1 > card2 and (card2, card1) != (0, 9)) or (card1 == 0 and card2 == 9)
+
+
+def play_game(
+        first_deck: Iterable[int],
+        second_deck: Iterable[int]
+) -> Sequence[int, int]:
+    first_deck: deque[int] = deque(first_deck)
+    second_deck: deque[int] = deque(second_deck)
+
+    rounds: int = 0
+    max_rounds: int = 1_000_000
 
     while first_deck and second_deck:
         rounds += 1
@@ -45,30 +56,21 @@ def play_game(first_deck: deque[str], second_deck: deque[str]) -> Union[str, Tup
             second_deck.extend([first_card, second_card])
 
         if rounds >= max_rounds:
-            return "botva"
+            return -1, -1
 
-    return "first" if first_deck else "second", rounds
-
-
-def is_first_player_winner(card1: str, card2: str) -> bool:
-    # Определяем победителя по правилам игры
-    return (card1 > card2 and (card2, card1) != ('0', '9')) or (card1 == '0' and card2 == '9')
+    return not bool(first_deck), rounds
 
 
 def main() -> None:
-    # Ввод данных
-    first_player_input = deque(input().split())
-    second_player_input = deque(input().split())
+    cases: Dict[int, str] = {-1: "botva", 0: "first", 1: "second"}
 
     # Запуск игры
-    result = play_game(first_player_input, second_player_input)
+    result: Sequence[int, int] = play_game(
+        map(int, input().split()),
+        map(int, input().split())
+    )
 
-    # Вывод результата
-    if result == "botva":
-        print(result)
-    else:
-        winner, rounds = result
-        print(winner, rounds)
+    print(cases[result[0]], result[1] if result[1] != -1 else "")
 
 
 if __name__ == '__main__':
