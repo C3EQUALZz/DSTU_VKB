520741cc0b9638f2619bdc245e5d51004e89c2e3
c3equalz <dan.kovalev2013@gmail.com>
2025-01-12 14:28:01 +0300

    Отрефакторил 1

--- programming_methods/laboratories/3/1.py
+++ programming_methods/laboratories/3/1.py
@@ -15,46 +15,65 @@
 
 Требуется вывести наибольшую общую подпоследовательность данных последовательностей, через пробел.
 """
-from typing import List
+from typing import List, Sequence
+from itertools import product
 
-def longest_common_subsequence(n: int, a: List[int], m: int, b: List[int]) -> List[int]:
-    """Находит наибольшую общую подпоследовательность двух последовательностей."""
+
+def find_longest_common_subsequence(
+        len_seq_a: int,
+        sequence_a: Sequence[int],
+        len_seq_b: int,
+        sequence_b: Sequence[int]
+) -> List[int]:
+    """
+    Находит наибольшую общую подпоследовательность двух последовательностей.
+
+    Принимает длины двух последовательностей и сами последовательности в виде списков целых чисел.
+    Создает двумерный массив `lcs_length_table`, где `lcs_length_table[i][j]` хранит длину наибольшей общей
+    подпоследовательности для первых `i` элементов первой последовательности и первых `j` элементов
+    второй последовательности.
+
+    Заполняет таблицу, сравнивая элементы последовательностей:
+    - Если элементы совпадают, увеличивает длину LCS на 1.
+    - Если не совпадают, берет максимальное значение из предыдущих вычислений.
+    Восстанавливает саму наибольшую общую подпоследовательность, проходя по таблице от конца к началу.
+    Возвращает LCS в правильном порядке.
+    """
     # Создаем таблицу для хранения длины LCS
-    dp = [[0] * (m + 1) for _ in range(n + 1)]
+    lcs_length_table: List[List[int]] = [[0] * (len_seq_b + 1) for _ in range(len_seq_a + 1)]
 
     # Заполняем таблицу
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            if a[i - 1] == b[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    for i, j in product(range(1, len_seq_a + 1), range(1, len_seq_b + 1)):
+        if sequence_a[i - 1] == sequence_b[j - 1]:
+            lcs_length_table[i][j] = lcs_length_table[i - 1][j - 1] + 1
+        else:
+            lcs_length_table[i][j] = max(lcs_length_table[i - 1][j], lcs_length_table[i][j - 1])
 
     # Восстанавливаем LCS
-    lcs = []
-    i, j = n, m
+    longest_common_subseq: List[int] = []
+    i: int = len_seq_a
+    j: int = len_seq_b
     while i > 0 and j > 0:
-        if a[i - 1] == b[j - 1]:
-            lcs.append(a[i - 1])
+        if sequence_a[i - 1] == sequence_b[j - 1]:
+            longest_common_subseq.append(sequence_a[i - 1])
             i -= 1
             j -= 1
-        elif dp[i - 1][j] == dp[i][j]:
+        elif lcs_length_table[i - 1][j] == lcs_length_table[i][j]:
             i -= 1
         else:
             j -= 1
 
-    return lcs[::-1]  # Возвращаем LCS в правильном порядке
+    return longest_common_subseq[::-1]
 
 
 def main() -> None:
-    """Основная функция для чтения входных данных и вывода результата."""
-    n = int(input())
-    a = list(map(int, input().split()))
-    m = int(input())
-    b = list(map(int, input().split()))
+    len_seq_a: int = int(input())
+    sequence_a: List[int] = list(map(int, input().split()))
+    len_seq_b: int = int(input())
+    sequence_b: List[int] = list(map(int, input().split()))
 
-    lcs = longest_common_subsequence(n, a, m, b)
-    print(' '.join(map(str, lcs)))
+    longest_common_subseq: List[int] = find_longest_common_subsequence(len_seq_a, sequence_a, len_seq_b, sequence_b)
+    print(' '.join(map(str, longest_common_subseq)))
 
 
 if __name__ == "__main__":
