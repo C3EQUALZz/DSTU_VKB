0824f0d407d85eb3b19ed2e5701e7bea190eb1c7
c3equalz <dan.kovalev2013@gmail.com>
2025-01-14 01:56:42 +0300

    Добавил 7 задание

--- programming_methods/laboratories/4/7.py
+++ programming_methods/laboratories/4/7.py
@@ -13,48 +13,66 @@
 В первой строке выведите "YES", если цикл существует, или "NO", в противном случае.
 При наличии цикла выведите во второй строке количество вершин в нем (считая одинаковые – первую и последнюю),
 а в третьей строке – вершины, входящие в этот цикл, в порядке обхода. Если циклов несколько, то выведите любой из них.
+
+НЕ ПРОХОДИТ НА 100 БАЛЛОВ ПО ПАМЯТИ!
 """
-INF = float('inf')
+from collections import deque, defaultdict
+from itertools import chain
+from typing import Final, Sequence
 
-n = int(input())
+INF: Final[float] = float('inf')
 
-adj_list = [
-    (w, (i, j))
-    for i in range(n)
-    for j, w in enumerate(map(int, input().split()))
-    if w < 100000
-]
 
-distance = [INF] * n
-used = [0] * n
-path = [-1] * n
-cycle = -1
+def restore_cycle(path: Sequence[int], start_index: int) -> Sequence[int]:
+    if start_index == -1:
+        return []
 
-for k in range(n):
-    if distance[k] == INF:
-        distance[k] = 0
-        for i in range(n):
-            cycle = -1
-            for j in range(len(adj_list)):
-                if distance[adj_list[j][1][0]] < INF:
-                    if distance[adj_list[j][1][1]] > distance[adj_list[j][1][0]] + adj_list[j][0]:
-                        distance[adj_list[j][1][1]] = distance[adj_list[j][1][0]] + adj_list[j][0]
-                        path[adj_list[j][1][1]] = adj_list[j][1][0]
-                        cycle = adj_list[j][1][0]
+    cycle_start = path[path.index(start_index)]
+    p = deque([cycle_start])
+    cycle = path[cycle_start]
 
-if cycle == -1:
-    print("NO")
-else:
-    print("YES")
-    cycle = path[path.index(cycle)]
-    cycle_start = cycle
-    p = [cycle]
-    cycle = path[cycle]
     while cycle != cycle_start:
-        p.append(cycle)
+        p.appendleft(cycle)
         cycle = path[cycle]
-    p.append(cycle_start)
-    print(len(p))
-    p.reverse()
-    for node in p:
-        print(node + 1, end=' ')
+
+    p.appendleft(cycle_start)
+
+    return p
+
+
+def main() -> None:
+    n = int(input())
+
+    graph = defaultdict(list)
+    for i in range(n):
+        for j, w in enumerate(map(int, input().split())):
+            if w < 100000:
+                graph[i].append((j, w))
+
+    distance = [INF] * n
+    path = [-1] * n
+    cycle = -1
+
+    # Алгоритм Беллмана-Форда
+    # Тут слишком сильные временные рамки, поэтому если выносить в отдельную функцию, то вместо 20 тестов будет 16 пройдено.
+    for k in filter(lambda x: distance[x] == INF, range(n)):
+        distance[k] = 0
+        for _ in range(n):
+            cycle = -1
+
+            for u, (v, weight) in chain.from_iterable(((u, edge) for edge in edges) for u, edges in graph.items()):
+                if distance[u] < INF and distance[v] > distance[u] + weight:
+                    distance[v] = distance[u] + weight
+                    path[v] = u
+                    cycle = v
+
+    result = restore_cycle(path, cycle)
+
+    if result:
+        print("YES", len(result), " ".join(str(node + 1) for node in result), sep="\n")
+    else:
+        print("NO")
+
+
+if __name__ == '__main__':
+    main()
