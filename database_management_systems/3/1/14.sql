55482661f2c55e189d8d900dc91659101ef321d2
c3equalz <dan.kovalev2013@gmail.com>
2025-02-01 00:37:21 +0300

    Добавил 14 задание

--- database_management_systems/3/1/14.sql
+++ database_management_systems/3/1/14.sql
@@ -1,3 +1,8 @@
+/*
+ Написать запрос, который выводит номер автомобиля и название его класса.
+ Если класс не определен, то вывести сообщение «класс не определен» (использовать left join).
+*/
+
 SELECT "AUTO"."INV", COALESCE("MENU"."NAME_REC", 'класс не определён')
 FROM "AUTO"
-LEFT JOIN "MENU" ON "AUTO"."C_CLASS" = ("MENU"."C_MENU"||"MENU"."C_REC")
+         LEFT JOIN "MENU" ON "AUTO"."C_CLASS" = ("MENU"."C_MENU" || "MENU"."C_REC")
