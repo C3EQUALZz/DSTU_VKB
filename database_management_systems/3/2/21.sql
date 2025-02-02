6f66fc136cfc4738f5dcff695079e5e61213f59e
matebookWindows <dan.kovalev2013@gmail.com>
2025-02-02 14:01:27 +0300

    Сделал 21 задание

--- database_management_systems/3/2/21.sql
+++ database_management_systems/3/2/21.sql
@@ -3,3 +3,32 @@
  за весь период обучения наибольший (без учета пустых значений).
  Список отсортировать по алфавиту. Вывести: Фамилию И.О., зачетная книжка, средний итоговый балл, позиция в списке 100.
  */
+
+WITH AverageMarksOfStudents AS (
+    SELECT
+        "StudentsT"."IDStudent"  AS student_id,
+        AVG(CAST("StudPlany"."ResultMark" AS NUMERIC)) AS average_mark_of_student
+    FROM "StudentsT" INNER JOIN "StudPlany" ON "StudentsT"."IDStudent" = "StudPlany"."Student"
+    GROUP BY "StudentsT"."IDStudent"
+),
+    RankedStudents AS (
+    SELECT
+        "Persons"."Фам",
+        "Persons"."Имя",
+        "Persons"."Отч",
+        "PersonT"."Номер зачетки",
+        ROW_NUMBER() OVER (ORDER BY "Persons"."Фам", "Persons"."Имя", "Persons"."Отч") AS position
+    FROM "Persons"
+    INNER JOIN AverageMarksOfStudents ON "Persons"."IDPersons" = AverageMarksOfStudents.student_id
+    INNER JOIN "PersonT" ON "Persons"."IDPersons" = "PersonT"."IDPerson"
+)
+
+SELECT
+    RankedStudents."Фам",
+    RankedStudents."Имя",
+    RankedStudents."Отч",
+    RankedStudents."Номер зачетки",
+    position
+FROM RankedStudents
+WHERE position <= 100
+ORDER BY RankedStudents."Фам", RankedStudents."Имя", RankedStudents."Отч";
