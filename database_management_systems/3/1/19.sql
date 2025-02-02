ff3681dbad0d096b63d23718a778929abd41e52e
matebookWindows <dan.kovalev2013@gmail.com>
2025-02-02 12:29:13 +0300

    Сделал 19 задание

--- database_management_systems/3/1/19.sql
+++ database_management_systems/3/1/19.sql
@@ -1,3 +1,8 @@
+/**
+  Вывести все автомобили, в номерах которых есть сочетание "3р", а номер тех. паспорта начинается
+  на "BMV".
+ */
+
 SELECT "INV","N_PASS", "SIGN"
 FROM "AUTO" 
-WHERE "N_PASS" LIKE 'BMV%' AND "SIGN" NOT LIKE '%3р%'
+WHERE "N_PASS" LIKE 'BMV%' AND "SIGN" LIKE '%3р%'
