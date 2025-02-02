6046b221cfe6f612ff5d13a9bdfb245dc056373e
c3equalz <dan.kovalev2013@gmail.com>
2025-02-02 15:47:28 +0300

    Сделал 23 задание

--- database_management_systems/3/2/23.sql
+++ database_management_systems/3/2/23.sql
@@ -1,20 +1,20 @@
 /*
- Для обучающихся студентов посчитать количество дисциплин (без повторов),
- которые будут им преподаваться в следующем семестре
- (текущий семестр установлен у объекта «Студент»).
+ По каждому человеку вывести количество всех зарегистрированных приказов
+ (т.е. дата регистрации не пустая), а также количеств зарегистрированных приказов,
+ которые были проведены.
  */
 
 SELECT "Persons"."IDPersons",
-COUNT(DISTINCT "Приказы"."Код Приказа"),
-COUNT(CASE WHEN "Приказы"."Проведен"='true' THEN 1 END)
-FROM "Persons" 
-JOIN "PersonT" 
-ON "Persons"."IDPersons"="PersonT"."IngPerson"
-JOIN "StudentsT"
-ON "PersonT"."IDPerson"="StudentsT"."IDPerson"
-JOIN "ПриказыПоСтудентам" 
-ON "StudentsT"."IDStudent"="ПриказыПоСтудентам"."КодСтудента"
-JOIN "Приказы"
-ON "ПриказыПоСтудентам"."КодПриказа"="Приказы"."Код Приказа"
+       COUNT(DISTINCT "Приказы"."Код Приказа") AS registered_orders,
+       COUNT(CASE WHEN "Приказы"."Проведен" = 'true' THEN 1 END) AS registered_orders_that_have_been_carried_out
+FROM "Persons"
+         JOIN "PersonT"
+              ON "Persons"."IDPersons" = "PersonT"."IngPerson"
+         JOIN "StudentsT"
+              ON "PersonT"."IDPerson" = "StudentsT"."IDPerson"
+         JOIN "ПриказыПоСтудентам"
+              ON "StudentsT"."IDStudent" = "ПриказыПоСтудентам"."КодСтудента"
+         JOIN "Приказы"
+              ON "ПриказыПоСтудентам"."КодПриказа" = "Приказы"."Код Приказа"
 WHERE "Приказы"."ДатаРегистрации" IS NOT NULL
 GROUP BY "Persons"."IDPersons"
