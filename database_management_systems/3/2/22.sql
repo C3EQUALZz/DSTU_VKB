916d5a6787c25b082fa01b9b54f5551078d10a1c
matebookWindows <dan.kovalev2013@gmail.com>
2025-02-02 14:30:19 +0300

    Сделал 22 задание

--- database_management_systems/3/2/22.sql
+++ database_management_systems/3/2/22.sql
@@ -2,3 +2,12 @@
  Для обучающихся студентов посчитать количество дисциплин (без повторов), которые будут им преподаваться в следующем
  семестре (текущий семестр установлен у объекта «Студент»).
  */
+
+SELECT
+    "StudentsT"."IDStudent" AS student_id,
+    COUNT(DISTINCT "Plany"."PDiscipline") AS count_of_subjects
+FROM "StudentsT"
+    INNER JOIN "StudPlany" ON "StudentsT"."IDStudent" = "StudPlany"."Student"
+    INNER JOIN "Plany" ON "StudPlany"."GroopPlan" = "Plany"."IDPlany"
+WHERE "StudentsT"."Semestr" + 1 = "Plany"."PSemestr"
+GROUP BY "StudentsT"."IDStudent";
