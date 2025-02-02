51a5578861028f57c10439a47bb4c47aafe13657
matebookWindows <dan.kovalev2013@gmail.com>
2025-02-02 12:29:03 +0300

    Сделал 18 задание

--- database_management_systems/3/1/18.sql
+++ database_management_systems/3/1/18.sql
@@ -2,3 +2,9 @@
  Вывести сумму, которую потратил каждый владелец на автомобили. Нулевые цены не учитывать.
  */
 
+SELECT
+    "AUTO"."C_OWNER" AS owner,
+    SUM("AUTO"."COST") AS summary_on_auto
+FROM "AUTO"
+WHERE "AUTO"."COST" IS NOT NULL AND "AUTO"."C_OWNER" IS NOT NULL
+GROUP BY "AUTO"."C_OWNER"
