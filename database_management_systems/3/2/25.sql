7f0de73efe01d1f2abeaa6447636f2aa3edb6be0
c3equalz <dan.kovalev2013@gmail.com>
2025-02-02 16:45:03 +0300

    Сделал 25 задание

--- database_management_systems/3/2/25.sql
+++ database_management_systems/3/2/25.sql
@@ -2,3 +2,18 @@
  Вывести преподавателей и дисциплины, по которым в таблице оценок для обучающихся студентов в текущем семестре есть
  незаполненные (пустые) оценки.
  */
+
+-- Тут очень странно поставлено условие. Тут нет такого случая, когда Mark может иметь NULL.
+
+SELECT
+    "Prepod"."PrepodFamIO" AS teacher_name,
+    "Plany"."PDiscipline" AS discipline_code
+FROM
+    "Mark"
+FULL JOIN "StudPlany" ON "Mark"."TStudPlan" = "StudPlany"."IDStudPlan"
+FULL JOIN "Plany" ON "StudPlany"."GroopPlan" = "Plany"."IDPlany"
+FULL JOIN "Prepod" ON "Plany"."IDPlany" = "Prepod"."PlanyID"
+FULL JOIN "StudentsT" ON "StudPlany"."Student" = "StudentsT"."IDStudent"
+WHERE
+    "Mark"."Mark" IS NULL AND "StudentsT"."Semestr" = "Plany"."PSemestr";
+
