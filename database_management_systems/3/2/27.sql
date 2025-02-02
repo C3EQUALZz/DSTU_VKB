7adf7d0275aa2e9f0879a12420c8afe3d9f58366
c3equalz <dan.kovalev2013@gmail.com>
2025-02-02 17:48:24 +0300

    Добавил 3 задания по базам данных

--- database_management_systems/3/2/27.sql
+++ database_management_systems/3/2/27.sql
@@ -5,8 +5,8 @@
 
 SELECT "IDPrepod", "PrepodFamIO", SUM("Plany"."PAuditTime")
 FROM "Prepod"
-JOIN "Plany" 
-ON "Prepod"."IDPrepod" = "Plany"."IDPlany"
+         JOIN "Plany"
+              ON "Prepod"."IDPrepod" = "Plany"."IDPlany"
 WHERE "Plany"."PSemestr" = 4
 GROUP BY "Prepod"."IDPrepod"
 ORDER BY SUM("Plany"."PAuditTime") DESC
--- database_management_systems/3/2/28.sql
+++ database_management_systems/3/2/28.sql
@@ -1,3 +1,17 @@
 /*
  Найти всех отчисленных студентов, у которых есть хотя бы одна задолженность.
  */
+
+SELECT
+    "StudentsT"."IDStudent" AS student_id,
+    "Persons"."Фам" || ' ' || "Persons"."Имя" || ' ' || "Persons"."Отч" AS student_name,
+    "StudPlany"."ResultMark" AS result_mark
+FROM
+    "StudentsT"
+JOIN "PersonT" ON "StudentsT"."IDPerson" = "PersonT"."IDPerson"
+JOIN "Persons" ON "PersonT"."IDPerson" = "Persons"."IDPersons"
+JOIN "StudPlany" ON "StudentsT"."IDStudent" = "StudPlany"."Student"
+WHERE
+    -- "StudentsT"."LearnStatus" = 'D'  -- статус отчисленных студентов (предположительно "D")
+    -- AND ("StudPlany"."ResultMark" IS NULL OR "StudPlany"."ResultMark" < 50);
+    "StudPlany"."ResultMark" IS NULL
