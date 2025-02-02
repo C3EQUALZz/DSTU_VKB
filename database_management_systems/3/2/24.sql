209cebe3ff90015270194a4d1087568294bc204e
c3equalz <dan.kovalev2013@gmail.com>
2025-02-02 15:47:33 +0300

    Сделал 24 задание

--- database_management_systems/3/2/24.sql
+++ database_management_systems/3/2/24.sql
@@ -3,3 +3,16 @@
  которые имеют эту страну в качестве своего адреса.
  Поиск проводить для всех мальчиков 4го курса и для девушек 9го семестра (только обучающиеся студенты).
  */
+
+SELECT
+    "Addresses"."AddrCountry" AS country,
+    COUNT("Persons"."IDPersons") AS count_of_students
+FROM "Persons"
+    INNER JOIN "Addresses" ON "Persons"."IDPersons" = "Addresses"."IDВладельца"
+    INNER JOIN "StudentsT" ON "Persons"."IDPersons" = "StudentsT"."IDPerson"
+WHERE
+    (("Persons"."Пол" = 'м' AND "StudentsT"."Semestr" IN (7, 8))
+    OR
+    ("Persons"."Пол" = 'ж' AND "StudentsT"."Semestr" = 9))
+GROUP BY "Addresses"."AddrCountry"
+
