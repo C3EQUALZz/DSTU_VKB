99913f0923ef325fd7eb0cbc9718e1a1e56c9601
matebookWindows <dan.kovalev2013@gmail.com>
2025-02-08 15:23:29 +0300

    Добавил логер в виды деревьев

--- programming_methods/practice/src/main/java/practice_2/question_2/BalancedTree.java
+++ programming_methods/practice/src/main/java/practice_2/question_2/BalancedTree.java
@@ -9,6 +9,7 @@
 
 package practice_2.question_2;
 
+import lombok.extern.slf4j.Slf4j;
 import practice_2.core.AbstractTree;
 import practice_2.core.Node;
 
@@ -16,6 +17,7 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class BalancedTree<T extends Comparable<T>> extends AbstractTree<T> {
 
     public BalancedTree(Collection<T> input) {
@@ -32,12 +34,17 @@ public class BalancedTree<T extends Comparable<T>> extends AbstractTree<T> {
     protected Node<T> buildTree(Collection<T> input) {
         var forest = input.stream().map(Node::new).collect(Collectors.toList());
 
+        log.debug("Initial start for balanced tree = {}", forest);
+
         while (forest.size() > 1) {
             var newForest = new LinkedList<Node<T>>();
             for (int i = 0; i < forest.size(); i += 2) {
                 var leftChild = forest.get(i);
                 var rightChild = i + 1 < forest.size() ? forest.get(i + 1) : null;
                 var value = (T) Character.valueOf('+');
+
+                log.debug("left child = {}, right child = {}", leftChild, rightChild);
+
                 newForest.add(new Node<>(value, leftChild, rightChild));
             }
             forest = newForest;
--- programming_methods/practice/src/main/java/practice_2/question_3/FullBalancedTree.java
+++ programming_methods/practice/src/main/java/practice_2/question_3/FullBalancedTree.java
@@ -8,6 +8,7 @@
 
 package practice_2.question_3;
 
+import lombok.extern.slf4j.Slf4j;
 import practice_2.core.AbstractTree;
 import practice_2.core.Node;
 
@@ -15,6 +16,7 @@ import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 public class FullBalancedTree<T extends Comparable<T>> extends AbstractTree<T> {
 
     public FullBalancedTree(Collection<T> input) {
@@ -24,6 +26,9 @@ public class FullBalancedTree<T extends Comparable<T>> extends AbstractTree<T> {
     @Override
     protected Node<T> buildTree(Collection<T> input) {
         var forest = input.stream().map(Node::new).collect(Collectors.toList());
+
+        log.debug("Initial collection = {}", forest);
+
         return buildTreeRecursive(forest, 0);
     }
 
@@ -31,6 +36,7 @@ public class FullBalancedTree<T extends Comparable<T>> extends AbstractTree<T> {
         if (index < symbols.size()) {
             int leftIndex = 2 * index + 1; // вычисление индекса для левого потомка
             int rightIndex = 2 * index + 2; // вычисление индекса для правого потомка
+            log.debug("Left index = {}, right index = {}", leftIndex, rightIndex);
 
             var value = symbols.get(index).getData();
             var leftChild = buildTreeRecursive(symbols, leftIndex);
