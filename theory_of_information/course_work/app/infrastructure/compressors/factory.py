fb09cd6096159ab1da1239b9cd78896e4c4ce9be
c3equalz <dan.kovalev2013@gmail.com>
2025-03-29 19:41:11 +0300

    booba

--- theory_of_information/course_work/app/infrastructure/compressors/factory.py
+++ theory_of_information/course_work/app/infrastructure/compressors/factory.py
@@ -2,6 +2,7 @@ from app.infrastructure.compressors.base import Compressor
 from app.infrastructure.compressors.bzip2 import Bzip2Compressor
 from app.infrastructure.compressors.gunzip import GunZipCompressor
 from app.infrastructure.compressors.pigz import PigzCompressor
+from app.infrastructure.compressors.xz import XzCompressor
 
 
 class CompressorFactory:
@@ -15,5 +16,8 @@ class CompressorFactory:
 
         if type_of_compressor in ("bz2", "bzip2"):
             return Bzip2Compressor()
+
+        if type_of_compressor == "xz":
+            return XzCompressor()
 
         raise TypeError(f"{type_of_compressor} is not a valid compressor")
