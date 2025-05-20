cb3fd19505fd3c2008ce8e91b97370896d433b3e
c3equalz <dan.kovalev2013@gmail.com>
2025-05-20 17:22:13 +0300

    Добаил замер метрик в микросервис пользователей, пока на данный момент не добавил .env и .env.example нужные значения

--- machine_learning/project/user-service/app/main.py
+++ machine_learning/project/user-service/app/main.py
@@ -1,16 +1,22 @@
+import logging
 from collections.abc import AsyncGenerator
 from contextlib import asynccontextmanager
+from typing import Final
 
 from dishka import AsyncContainer
 from dishka.integrations.fastapi import setup_dishka as setup_dishka_fastapi
+from dishka.integrations.faststream import setup_dishka as setup_dishka_faststream
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from faststream import FastStream
 from faststream.asgi import make_ping_asgi, make_asyncapi_asgi
+from faststream.kafka import KafkaBroker
 from sqlalchemy.ext.asyncio import AsyncEngine, create_async_engine
 from sqlalchemy.orm import clear_mappers
 
-from app.application.api.v1.users.handlers import router as users_router
+from app.application.api.middlewares.metrics import HTTPLatencyMetricsMiddleware
 from app.application.api.v1.auth.handlers import router as auth_router
+from app.application.api.v1.users.handlers import router as users_router
 from app.application.api.v1.utils.handlers import register_exception_handlers
 from app.infrastructure.adapters.alchemy.orm import metadata, start_mappers
 from app.infrastructure.brokers.base import BaseMessageBroker
@@ -18,19 +24,29 @@ from app.logic.container import get_container
 from app.settings.config import Settings, get_settings
 from app.settings.logger.config import setup_logging
 
+logger: Final[logging.Logger] = logging.getLogger(__name__)
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     settings: Settings = get_settings()
     container: AsyncContainer = get_container()
     broker: BaseMessageBroker = await container.get(BaseMessageBroker)
+    faststream_broker: KafkaBroker = await container.get(KafkaBroker)
+    faststream_app: FastStream = FastStream(faststream_broker, logger=logger)
+    setup_dishka_faststream(container, FastStream(faststream_broker, logger=logger), auto_inject=True)
+
     # cache.pool = await container.get(ConnectionPool)
     # cache.client = await container.get(Redis)
+
     engine: AsyncEngine = create_async_engine(settings.database.url)
     async with engine.begin() as conn:
         await conn.run_sync(metadata.create_all)
 
     start_mappers()
+
+    app.mount("/health", make_ping_asgi(faststream_broker, timeout=5.0))
+    app.mount("/asyncapi", make_asyncapi_asgi(faststream_app))
 
     await broker.start()
 
@@ -44,6 +60,7 @@ async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
 
 
 def create_app() -> FastAPI:
+    settings: Settings = get_settings()
     container: AsyncContainer = get_container()
 
     app = FastAPI(
@@ -56,6 +73,19 @@ def create_app() -> FastAPI:
 
     setup_logging()
     register_exception_handlers(app)
+
+    app.add_middleware(
+        CORSMiddleware,  # type: ignore
+        allow_origins=settings.cors.allow_origins,
+        allow_credentials=settings.cors.allow_credentials,
+        allow_methods=settings.cors.allow_methods,
+        allow_headers=settings.cors.allow_headers,
+    )
+
+    app.add_middleware(
+        HTTPLatencyMetricsMiddleware,  # type: ignore
+        container=container
+    )
 
     app.include_router(users_router)
     app.include_router(auth_router)
