de96e7e4468bfb73c3283bf08293580205a3b7b9
matebookWindows <dan.kovalev2013@gmail.com>
2025-05-21 08:12:52 +0300

    added service for user

--- protection_in_databases/2/app/exceptions/infrastructure.py
+++ protection_in_databases/2/app/exceptions/infrastructure.py
@@ -15,3 +15,17 @@ class InfrastructureException(BaseAppError, ABC):
 @dataclass(eq=False)
 class TypeSignatureError(InfrastructureException):
     ...
+
+
+@dataclass(eq=False)
+class UserNotFoundError(InfrastructureException):
+    @property
+    def status(self) -> int:
+        return HTTPStatus.NOT_FOUND.value
+
+
+@dataclass(eq=False)
+class MissingAttributeError(InfrastructureException):
+    @property
+    def status(self) -> int:
+        return HTTPStatus.INTERNAL_SERVER_ERROR.value
