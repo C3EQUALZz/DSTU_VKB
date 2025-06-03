735e27aa60175cd5ae35b74631719e8e8a5980f3
c3equalz <dan.kovalev2013@gmail.com>
2025-06-03 03:06:55 +0300

    Начал пока делать 3 лабу по Куликовой

---
+++ software_reliability/practics/3/1/1.py
@@ -0,0 +1,69 @@
+from abc import abstractmethod
+from dataclasses import dataclass
+from typing import Protocol, List, override
+
+
+class RadioStation(Protocol):
+    @abstractmethod
+    def is_in_frequency_range(
+            self,
+            min_freq: float,
+            max_freq: float
+    ) -> bool:
+        """Проверяет, находится ли частота станции в заданном диапазоне"""
+        raise NotImplementedError
+
+
+@dataclass
+class SimpleRadioStation(RadioStation):
+    name: str
+    frequency: float
+
+    @override
+    def is_in_frequency_range(self, min_freq: float, max_freq: float) -> bool:
+        return min_freq <= self.frequency <= max_freq
+
+
+class RadioStationManager:
+    def __init__(self) -> None:
+        self._stations: List[RadioStation] = []
+
+    def add_station(self, station: RadioStation) -> None:
+        self._stations.append(station)
+
+    def count_in_frequency_range(self, min_freq: float, max_freq: float) -> int:
+        return sum(1 for station in self._stations if station.is_in_frequency_range(min_freq, max_freq))
+
+
+def main() -> None:
+    manager: RadioStationManager = RadioStationManager()
+
+    while True:
+        try:
+            n: int = int(input("Введите количество радиостанций: "))
+
+            for i in range(n):
+                name: str = input(f"Радиостанция {i + 1}. Наименование: ")
+
+                while True:
+                    try:
+                        freq: float = float(input("Частота: "))
+                        break
+                    except ValueError:
+                        print("Введите число в формате 100.5")
+
+                manager.add_station(SimpleRadioStation(name, freq))
+
+            print("\nВведите диапазон частот для поиска:")
+            min_freq: float = float(input("Минимальная частота: "))
+            max_freq: float = float(input("Максимальная частота: "))
+
+            result = manager.count_in_frequency_range(min_freq, max_freq)
+            print(f"\nКоличество радиостанций в диапазоне: {result}")
+
+        except ValueError as e:
+            print(f"Ошибка ввода: {e}")
+
+
+if __name__ == "__main__":
+    main()
